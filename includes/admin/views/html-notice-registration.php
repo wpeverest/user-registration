024f2b80d09876a0464dae457582d39a924d0259
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-09-07 14:53:22 +0545

    Beta and notice fix

--- includes/admin/views/html-notice-registration.php
+++ includes/admin/views/html-notice-registration.php
@@ -11,6 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div id="message" class="error user-registration-message">
 	<a class="user-registration-message-close notice-dismiss" href="<?php echo esc_url( wp_nonce_url( add_query_arg( 'ur-hide-notice', 'register' ), 'user_registration_hide_notices_nonce', '_ur_notice_nonce' ) ); ?>"><?php _e( 'Dismiss', 'user-registration' ); ?></a>
 
- 	<p><?php echo sprintf( __( 'Please enable <strong>Anyone can register</strong> option on %sgeneral setting%s.', 'user-registration' ), '<a target="_blank" href="' . admin_url( 'options-general.php#admin_email' ) . '">', '</a>' ); ?></p>
+	<p><?php echo sprintf( __( 'To allow users to register for your website via User registration, you must first enable user registration. Go to %sSettings > General%s tab, and under Membership make sure to check <strong>Anyone can register</strong>.', 'user-registration' ), '<a target="_blank" href="' . admin_url( 'options-general.php#admin_email' ) . '">', '</a>' ); ?></p>
 </div>
 
