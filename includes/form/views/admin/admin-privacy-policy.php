156052508b372693a8f91e0f095e78baae60aac2
sanjeev3 <sanjeev.themegrill@gmail.com>
2017-12-25 11:22:23 +0545

    Privacy policy in users tab

--- includes/form/views/admin/admin-privacy-policy.php
+++ includes/form/views/admin/admin-privacy-policy.php
@@ -8,21 +8,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 ?>
-<div class="ur-input-type-date ur-admin-template">
+<div class="ur-input-type-privacy-policy ur-admin-template">
 
 	<div class="ur-label">
 		<label><?php echo esc_html($this->get_general_setting_data( 'label' )); ?></label>
 
 	</div>
-	<div class="ur-field" data-field-key="date">
+	<div class="ur-field" data-field-key="privacy_policy">
 
-		<input type="checkbox" id="ur-input-type-date"
+		<input type="checkbox" id="ur-input-type-privacy-policy"
 		       placeholder="<?php echo esc_attr($this->get_general_setting_data( 'placeholder' )); ?>"/>
 
 	</div>
 	<?php
 
-	UR_PrivacyPolicy::get_instance()->get_setting();
+	UR_Privacy_Policy::get_instance()->get_setting();
 
 	?>
 </div>
