970f1070eb6e0ce53433c11bbffe804c7485c08a
sanjeev3 <sanjeev.themegrill@gmail.com>
2018-04-10 11:57:00 +0545

    Add js validate

--- assets/js/frontend/user-registration.js
+++ assets/js/frontend/user-registration.js
@@ -2,6 +2,139 @@
 /* global  ur_google_recaptcha_code */
 /* global  grecaptcha */
 (function ( $ ) {
+	var user_registration = {
+		$user_registration: $( 'form.register' ),
+		init: function() {
+			this.init_datepicker();
+			this.load_validation();
+
+			// Inline validation
+			this.$user_registration.on( 'input validate change', '.input-text, select, input:checkbox', this.validate_field );
+		},
+		init_datepicker: function () {
+			$( '.date-picker-field, .date-picker' ).datepicker({
+				changeMonth: true,
+				changeYear: true,
+				defaultDate: '',
+				dateFormat: 'yy-mm-dd',
+				numberOfMonths: 1,
+				minDate: '-15Y',
+				maxDate: '+15Y'
+			});
+		},
+		load_validation: function() {
+			if ( typeof $.fn.validate === 'undefined' ) {
+				return false;
+			}
+
+			// Validator messages.
+			$.extend( $.validator.messages, {
+				required: user_registration_params.i18n_messages_required,
+				url: user_registration_params.i18n_messages_url,
+				email: user_registration_params.i18n_messages_email,
+				number: user_registration_params.i18n_messages_number
+			});
+
+			// Validate email addresses.
+			$.validator.methods.email = function( value, element ) {
+				/* https://stackoverflow.com/questions/2855865/jquery-validate-e-mail-address-regex */
+				var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+				return this.optional( element ) || pattern.test( value );
+			};
+
+			this.$user_registration.each( function() {
+				var $this = $( this );
+
+				$this.validate({
+					errorClass: 'user-registration-error',
+					validClass: 'user-registration-valid',
+					errorPlacement: function( error, element ) {
+						if ( 'radio' === element.attr( 'type' ) || 'checkbox' === element.attr( 'type' ) ) {
+							element.parent().parent().parent().append( error );
+						} else if ( element.is( 'select' ) && element.attr( 'class' ).match( /date-month|date-day|date-year/ ) ) {
+							if ( element.parent().find( 'label.user-registration-error:visible' ).length === 0 ) {
+								element.parent().find( 'select:last' ).after( error );
+							}
+						} else {
+							error.insertAfter( element );
+						}
+					},
+					highlight: function( element, errorClass, validClass ) {
+						var $element  = $( element ),
+							$parent   = $element.closest( '.form-row' ),
+							inputName = $element.attr( 'name' );
+
+						if ( $element.attr( 'type' ) === 'radio' || $element.attr( 'type' ) === 'checkbox' ) {
+							$parent.find( 'input[name=\''+inputName+'\']' ).addClass( errorClass ).removeClass( validClass );
+						} else {
+							$element.addClass( errorClass ).removeClass( validClass );
+						}
+
+						$parent.removeClass( 'user-registration-validated' ).addClass( 'user-registration-invalid user-registration-has-error' );
+					},
+					unhighlight: function( element, errorClass, validClass ) {
+						var $element  = $( element ),
+							$parent   = $element.closest( '.form-row' ),
+							inputName = $element.attr( 'name' );
+
+						if ( $element.attr( 'type' ) === 'radio' || $element.attr( 'type' ) === 'checkbox' ) {
+							$parent.find( 'input[name=\''+inputName+'\']' ).addClass( validClass ).removeClass( errorClass );
+						} else {
+							$element.addClass( validClass ).removeClass( errorClass );
+						}
+
+						$parent.removeClass( 'user-registration-has-error' );
+					},
+					submitHandler: function( errorClass, form ) {
+               			form.submit();
+					}
+				});
+			});
+		},
+		validate_field: function ( e ) {
+			var $this             = $( this ),
+				$parent           = $this.closest( '.form-row' ),
+				validated         = true,
+				validate_required = $parent.is( '.validate-required' ),
+				validate_email    = $parent.is( '.validate-email' ),
+				event_type        = e.type;
+
+			if ( 'input' === event_type ) {
+				$parent.removeClass( 'everest-forms-invalid everest-forms-invalid-required-field everest-forms-invalid-email everest-forms-validated' );
+			}
+
+			if ( 'validate' === event_type || 'change' === event_type ) {
+
+				if ( validate_required ) {
+					if ( 'checkbox' === $this.attr( 'type' ) && ! $this.is( ':checked' ) ) {
+						$parent.removeClass( 'everest-forms-validated' ).addClass( 'everest-forms-invalid everest-forms-invalid-required-field' );
+						validated = false;
+					} else if ( $this.val() === '' ) {
+						$parent.removeClass( 'everest-forms-validated' ).addClass( 'everest-forms-invalid everest-forms-invalid-required-field' );
+						validated = false;
+					}
+				}
+
+				if ( validate_email ) {
+					if ( $this.val() ) {
+						/* https://stackoverflow.com/questions/2855865/jquery-validate-e-mail-address-regex */
+						var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+
+						if ( ! pattern.test( $this.val()  ) ) {
+							$parent.removeClass( 'everest-forms-validated' ).addClass( 'everest-forms-invalid everest-forms-invalid-email' );
+							validated = false;
+						}
+					}
+				}
+
+				if ( validated ) {
+					$parent.removeClass( 'everest-forms-invalid everest-forms-invalid-required-field everest-forms-invalid-email' ).addClass( 'everest-forms-validated' );
+				}
+			}
+		}
+	};
+	user_registration.init();
+
 	var ursL10n = user_registration_params.ursL10n;
 
 	$.fn.ur_form_submission = function () {
@@ -33,7 +166,6 @@
 							}
 						}
 					});
-
 
 					for ( var multi_start = 0; multi_start < multi_value_field.length; multi_start++ ) {
 
