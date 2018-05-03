68f53601434ff618cde7ad8fc5853090b6e9d489
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2018-05-03 12:12:28 +0545

    Introduce reset password email customization

--- includes/admin/settings/class-ur-settings-email.php
+++ includes/admin/settings/class-ur-settings-email.php
@@ -55,6 +55,8 @@ if ( ! class_exists( 'UR_Settings_Email' ) ) :
 
 			$this->emails['UR_Settings_Successfully_Registered_Email'] = include( 'emails/class-ur-settings-successfully-registered-email.php' );
 
+			$this->emails['UR_Settings_Reset_Password_Email'] = include( 'emails/class-ur-settings-reset-password-email.php' );
+
 			$this->emails = apply_filters( 'user_registration_email_classes', $this->emails );
 		}
 
---
+++ includes/admin/settings/emails/class-ur-settings-reset-password-email.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Configure Email
+ *
+ * @class    UR_Settings_Reset_Password_Email
+ * @category Class
+ * @author   WPEverest
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'UR_Settings_Reset_Password_Email', false ) ) :
+
+/**
+ * UR_Settings_Reset_Password_Email Class.
+ */
+class UR_Settings_Reset_Password_Email{
+	
+	public function __construct() {
+		$this->id             = 'reset_password_email';
+		$this->title          = __( 'Reset Password Email', 'user-registration' );
+		$this->description    = __( 'Email sent to the user when a user requests for reset password', 'user-registration' );
+	}
+
+		/**
+		 * Get settings
+		 *
+		 * @return array
+		 */
+		public function get_settings() {
+
+		?><h2><?php echo esc_html__('Reset Password Email','user-registration'); ?> <?php ur_back_link( __( 'Return to emails', 'user-registration' ), admin_url( 'admin.php?page=user-registration-settings&tab=email' ) ); ?></h2>
+
+		<?php
+			$settings = apply_filters(
+				'user_registration_reset_password_email', array(
+
+					array(
+						'type'  => 'title',
+						'desc'  => '',
+						'id'    => 'reset_password_email',
+					),
+					array(
+						'title'    => __( 'Enable this email', 'user-registration' ),
+						'desc'     => __( 'Enable this email sto the user when a user requests for reset password.', 'user-registration' ),
+						'id'       => 'user_registration_enable_reset_password_email',
+						'default'  => 'yes',
+						'type'     => 'checkbox',
+						'autoload' => false,
+					),
+					array(
+						'title'    => __( 'Email Subject', 'user-registration' ),
+						'desc'     => __( 'The email subject you want to customize.', 'user-registration' ),
+						'id'       => 'user_registration_reset_password_email_subject',
+		 				'type'     => 'text',
+		 				'default'  => __('Password Reset Email: {{blog_info}}', 'user-registration'),
+						'css'      => 'min-width: 350px;',
+						'desc_tip' => true,
+					),
+
+					array(
+						'title'    => __( 'Email Content', 'user-registration' ),
+						'desc'     => __( 'The email content you want to customize.', 'user-registration' ),
+						'id'       => 'user_registration_reset_password_email',
+		 				'type'     => 'tinymce',
+		 				'default'  => $this->ur_get_reset_password_email(),
+						'css'      => 'min-width: 350px;',
+						'desc_tip' => true,
+					),
+
+					array(
+						'type' => 'sectionend',
+						'id'   => 'reset_password_email',
+					),
+				)
+			);
+
+			return apply_filters( 'user_registration_get_settings_' . $this->id, $settings );
+		}
+
+	public function ur_get_reset_password_email() {
+	
+		$message = apply_filters( 'user_registration_reset_password_email_message', __( sprintf(
+			'Someone has requested a password reset for the following account:
+			<br/><br/> {{home_url}} Username: {{username}}<br/>
+			If this was a mistake, just ignore this email and nothing will happen. <br/>
+			To reset your password, visit the following address:<br/>
+			{{home_url}}/wp-login.php?action=rp&key={{key}}&login={{username}}<br/>
+			Thank You!'), 'user-registration' ) );
+
+		return $message;
+	}
+}
+endif;
+
+return new UR_Settings_Reset_Password_Email();
