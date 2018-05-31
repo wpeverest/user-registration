c83a267ac5c486c288484d3bca7b09ed68f52945
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2018-05-31 17:34:29 +0545

    Add missing placeholder for default email

--- includes/form/views/admin/admin-email.php
+++ includes/form/views/admin/admin-email.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div>
 	<div class="ur-field" data-field-key="email">
 
-		<input type="email" value="<?php echo esc_attr( $this->get_advance_setting_data( 'default_value' )); ?>" id="ur-input-type-email"/>
+		<input type="email" placeholder="<?php echo esc_attr($this->get_general_setting_data( 'placeholder' )); ?>" value="<?php echo esc_attr( $this->get_advance_setting_data( 'default_value' )); ?>" id="ur-input-type-email"/>
 
 	</div>
 	<?php
