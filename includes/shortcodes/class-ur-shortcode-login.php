dd1b9f2f34b2422e4ccd77d19408179e7434a471
ThemeGrill <snrbnod@gmail.com>
2018-06-07 16:39:08 +0545

    enable recaptcha in login form

--- includes/shortcodes/class-ur-shortcode-login.php
+++ includes/shortcodes/class-ur-shortcode-login.php
@@ -40,20 +40,18 @@ class UR_Shortcode_Login {
 	public static function output( $atts ) {
 		global $wp, $post;
 
-		$redirect_url = isset( $atts['redirect_url']) ? $atts['redirect_url'] : '';
+		$redirect_url = isset( $atts['redirect_url'] ) ? $atts['redirect_url'] : '';
+		$enable_recaptcha = isset( $atts['enable_recaptcha'] ) ? $atts['enable_recaptcha'] : '';
 
 		if ( ! is_user_logged_in() ) {
-
 			if ( isset( $wp->query_vars['lost-password'] ) ) {
 				UR_Shortcode_My_Account::lost_password();
 			} else {
-
-				ur_get_template( 'myaccount/form-login.php' );
+				ur_get_template( 'myaccount/form-login.php', set_query_var( "recaptcha", $enable_recaptcha )
+				);
 			}
-
 		}
-		else
-		{
+		else {
 			echo sprintf( __('You are already logged in. <a href="%s">Log out?</a>', 'user-registration' ),  ur_logout_url() ) ;
 		}
 	}
