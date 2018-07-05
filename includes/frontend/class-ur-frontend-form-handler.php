1232adacff541c29fbed64f93db5c67af4c304b1
Sanzeeb Aryal <sanzeeb.aryal@gmail.com>
2018-07-05 10:54:04 +0545

    BuddyPress compatibiltiy on members area

--- includes/frontend/class-ur-frontend-form-handler.php
+++ includes/frontend/class-ur-frontend-form-handler.php
@@ -268,6 +268,7 @@ class UR_Frontend_Form_Handler {
 				update_user_meta( $user_id, $field_name, $data->value );
 			}
 		update_user_meta( $user_id, 'ur_form_id', $form_id );
+		update_user_meta( $user_id, 'last_activity', '' ); //BuddyPress members area compatibility.
 		}
 	}
 
