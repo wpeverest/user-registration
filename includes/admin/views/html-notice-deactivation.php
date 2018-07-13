bdf7dca803751c1152f8a3ce765f42b7aece7a20
Sanjeev <sanzeeb.aryal@gmail.com>
2018-07-13 12:42:57 +0545

    Deactivation notice complete

---
+++ includes/admin/views/html-notice-deactivation.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Deactivation admin notice
+ *
+ * Link to settings page.
+ *
+ * @author      WPEverest
+ * @category    Admin
+ * @package     User Registration/Admin
+ * @since    	1.1.4
+ */
+
+$reason_deactivation_url = 'http://wpeverest.com/contact';
+global $status, $page, $s;
+
+$deactivate_url = wp_nonce_url( 'plugins.php?action=deactivate&amp;plugin=' . UR_PLUGIN_BASENAME . '&amp;plugin_status=' . $status . '&amp;paged=' . $page . '&amp;s=' . $s, 'deactivate-plugin_' . UR_PLUGIN_BASENAME );
+?>
+<tr class="plugin-update-tr active updated" data-slug="user-registration" data-plugin="user-registration/user-registration.php">
+	<td colspan ="3" class="plugin-update colspanchange">
+		<div class="notice inline notice-alt notice-warning">
+			<p><?php printf( __( 'Before we deactivate User Registration, would you care to <a href="%1$s" target="_blank">let us know why</a> so we can improve it for you? <a href="%2$s">No, deactivate now</a>.', 'user-registration' ), $reason_deactivation_url, $deactivate_url ); ?></p>
+		</div>
+	</td>
+</tr>
