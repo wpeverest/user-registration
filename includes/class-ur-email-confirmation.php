476bbeb6c2d69d50b0e1ffd4a01dce81d064ec48
Sanjeev <sanzeeb.aryal@gmail.com>
2018-08-09 10:22:05 +0545

    Send empty array as attachment while rending token

--- includes/class-ur-email-confirmation.php
+++ includes/class-ur-email-confirmation.php
@@ -199,7 +199,9 @@ class UR_Email_Confirmation {
 				$this->get_token( $user_id );
 				$this->set_email_status( array(), '', $user_id );
 
-				UR_Emailer::send_mail_to_user( $user->user_email, $user->user_login, $user_id, '', array() );
+				$attachments = apply_filters( 'user_registration_email_attachment_resending_token', array() );
+
+				UR_Emailer::send_mail_to_user( $user->user_email, $user->user_login, $user_id, '', array(), $attachments );
 				
 				add_filter('login_message', array( $this,'custom_resend_email_token_message' ) );
 				add_filter('user_registration_login_form_before_notice', array( $this,'custom_resend_email_token_message' ) );
