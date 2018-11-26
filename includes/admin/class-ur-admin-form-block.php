277c9260c9dcd150dfd5872127b615c03b59c5d2
Sanjeev <sanzeeb.aryal@gmail.com>
2018-11-26 12:14:40 +0545

    Enqueue JS for gutenberg block

--- includes/admin/class-ur-admin-form-block.php
+++ includes/admin/class-ur-admin-form-block.php
@@ -2,7 +2,7 @@
 
 /**
  * Form Selector Gutenberg block with live preview.
- * @since      1.5.2
+ * @since      1.5.1
  */
 class UR_Form_Block {
 
@@ -12,15 +12,42 @@ class UR_Form_Block {
 	public function __construct() {
 
 		add_action( 'init', array( $this, 'register_block' ) );
+		add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ) );
+	}
+
+	/**
+	 * Enqueue Block Editor Assets.
+	 * @return void.
+	 */
+	public function enqueue_block_editor_assets() {
+
+		$suffix    = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
+		wp_register_script(
+	        'user-registration-block-editor',
+	        UR()->plugin_url() . '/assets/js/admin/form-block' . $suffix . '.js',
+	        array( 'wp-blocks', 'wp-element' )
+		);
+
+		wp_enqueue_script( 'user-registration-block-editor' );
 	}
 
 	/**
 	 * Register user registration Gutenberg block on the backend.
 	 *
-	 * @since 1.5.2
+	 * @since 1.5.1
 	 */
 	public function register_block() {
 
+		register_block_type( 'user-registration/form-selector', array(
+			'attributes'      => array(
+				'formId'       => array(
+					'type' => 'string',
+				),
+			),
+			'editor_script'   => 'user-registration-block-editor',
+			'render_callback' => array( $this, 'get_form_html' ),
+		) );
 	}
 }
 
