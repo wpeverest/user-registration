1c7be8e112993ceba0a1a4e1c2a255f39e62bbc0
Sanjeev <sanzeeb.aryal@gmail.com>
2018-12-13 12:12:13 +0545

    Add date field in settings

--- includes/admin/class-ur-admin-settings.php
+++ includes/admin/class-ur-admin-settings.php
@@ -318,6 +318,29 @@ class UR_Admin_Settings {
 					</tr><?php
 					break;
 
+				case 'date' :
+					$option_value = self::get_option( $value['id'], $value['default'] );
+
+					?><tr valign="top" class="<?php echo esc_attr( $value['row_class'] ); ?>">
+						<th scope="row" class="titledesc">
+							<label for="<?php echo esc_attr( $value['id'] ); ?>"><?php echo esc_html( $value['title'] ); ?></label>
+							<?php echo $tooltip_html; ?>
+						</th>
+						<td class="forminp forminp-<?php echo sanitize_title( $value['type'] ) ?>">
+							<input
+								name="<?php echo esc_attr( $value['id'] ); ?>"
+								id="<?php echo esc_attr( $value['id'] ); ?>"
+								type="<?php echo esc_attr( $value['type'] ); ?>"
+								style="<?php echo esc_attr( $value['css'] ); ?>"
+								value="<?php echo esc_attr( $option_value ); ?>"
+								class="<?php echo esc_attr( $value['class'] ); ?>"
+								placeholder="<?php echo esc_attr( $value['placeholder'] ); ?>"
+								<?php echo implode( ' ', $custom_attributes ); ?>
+								/> <?php echo $description; ?>
+						</td>
+					</tr><?php
+					break;
+
 				// Color picker.
 				case 'color' :
 					$option_value = self::get_option( $value['id'], $value['default'] );
