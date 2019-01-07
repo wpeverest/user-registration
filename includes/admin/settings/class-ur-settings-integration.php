9a95d4261361e2a03e2ae41411522c1d1b8e2581
Sanjeev Aryal <sanzeeb.aryal@gmail.com>
2019-01-07 11:07:49 +0545

    Version update 1.5.5 and grunt tasks

--- includes/admin/settings/class-ur-settings-integration.php
+++ includes/admin/settings/class-ur-settings-integration.php
@@ -50,7 +50,8 @@ if ( ! class_exists( 'UR_Settings_Integration ' ) ) :
 					),
 					array(
 						'title'    => __( 'Site Key', 'user-registration' ),
-						'desc'     => sprintf( __('Get site key from google %1$s reCaptcha %2$s.', 'user-registration' ), '<a href="https://www.google.com/recaptcha" target="_blank">', '</a>' ),						'id'       => 'user_registration_integration_setting_recaptcha_site_key',
+						'desc'     => sprintf( __('Get site key from google %1$s reCaptcha %2$s.', 'user-registration' ), '<a href="https://www.google.com/recaptcha" target="_blank">', '</a>' ),
+						'id'       => 'user_registration_integration_setting_recaptcha_site_key',
 						'default'  => '',
 						'type'     => 'text',
 						'class'    => '',
