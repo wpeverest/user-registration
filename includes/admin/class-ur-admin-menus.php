74653e762e8c30a874bcbfec38612d280562666c
Nilan Bhasima <nilanbhas@gmail.com>
2019-04-04 17:08:48 +0545

    Added svg logo for dashboard menu

--- includes/admin/class-ur-admin-menus.php
+++ includes/admin/class-ur-admin-menus.php
@@ -273,7 +273,8 @@ if ( ! class_exists( 'UR_Admin_Menus', false ) ) :
 		 * @return string
 		 */
 		private function get_icon_svg( $base64 = true ) {
-			$svg = '';
+			$svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="#82878c" d="M27.58 4a27.9 27.9 0 0 0-5.17 4 27 27 0 0 0-4.09 5.08 33.06 33.06 0 0 1 2 4.65A23.78 23.78 0 0 1 24 12.15V18a8 8 0 0 1-5.89 7.72l-.21.05a27 27 0 0 0-1.9-8.16A27.9 27.9 0 0 0 9.59 8a27.9 27.9 0 0 0-5.17-4L4 3.77V18a12 12 0 0 0 9.93 11.82h.14a11.72 11.72 0 0 0 3.86 0h.14A12 12 0 0 0 28 18V3.77zM8 18v-5.85a23.86 23.86 0 0 1 5.89 13.57A8 8 0 0 1 8 18zm8-16a3 3 0 1 0 3 3 3 3 0 0 0-3-3z"/></svg>';
+
 
 			if ( $base64 ) {
 				return 'data:image/svg+xml;base64,' . base64_encode( $svg );
