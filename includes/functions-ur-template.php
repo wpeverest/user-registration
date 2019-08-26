7738bca068561d8d4507f66a2126ed8dbbbee34c
Rumesh Udash <allmesh67@gmail.com>
2019-08-26 15:32:29 +0545

    Fix - Double description in checkbox

--- includes/functions-ur-template.php
+++ includes/functions-ur-template.php
@@ -258,9 +258,6 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 
 					$field  = '<label class="ur-label" ' . implode( ' ', $custom_attributes ) . '">';
 					$field .= $args['label'] . $required . '</label>';
-					if ( $args['description'] ) {
-						$field .= '<span class="description">' . $args['description'] . '</span>';
-					}
 
 					$checkbox_start = 0;
 
@@ -284,9 +281,6 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 					$field = '<label class="ur-label checkbox ' . implode( ' ', $custom_attributes ) . '">
 							<input data-rules="' . esc_attr( $rules ) . '" data-id="' . esc_attr( $key ) . '" ' . implode( ' ', $custom_attributes ) . ' data-value="' . $value . '" type="' . esc_attr( $args['type'] ) . '" class="input-checkbox ' . esc_attr( implode( ' ', $args['input_class'] ) ) . '" name="' . esc_attr( $key ) . '" id="' . esc_attr( $args['id'] ) . '" value="1" ' . checked( $value, 1, false ) . ' /> '
 							 . $args['label'] . $required . '</label>';
-					if ( $args['description'] ) {
-						$field .= '<span class="description">' . $args['description'] . '</span>';
-					}
 				}
 				break;
 
