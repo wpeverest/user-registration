a12ba12f7c09f625defcbce35d2148001050ea91
unknown <lekhnath005@gmail.com>
2019-11-14 10:52:52 +0545

    remove date range validation

--- includes/form/class-ur-form-field-date.php
+++ includes/form/class-ur-form-field-date.php
@@ -52,6 +52,7 @@ class UR_Form_Field_Date extends UR_Form_Field {
 	public function validation( $single_form_field, $form_data, $filter_hook, $form_id ) {
 
 		$is_condition_enabled = isset( $single_form_field->advance_setting->enable_conditional_logic ) ? $single_form_field->advance_setting->enable_conditional_logic : '0';
+		$is_enable_date_range = isset( $single_form_field->advance_setting->enable_date_range ) ? $single_form_field->advance_setting->enable_date_range : '';
 		$required             = isset( $single_form_field->general_setting->required ) ? $single_form_field->general_setting->required : 'no';
 		$field_label          = isset( $form_data->label ) ? $form_data->label : '';
 		$value                = isset( $form_data->value ) ? $form_data->value : '';
@@ -64,8 +65,7 @@ class UR_Form_Field_Date extends UR_Form_Field {
 				}
 			);
 		}
-
-		if ( ! $this->is_valid_date( $value ) && ! empty( $value ) ) {
+		if ( ! $this->is_valid_date( $value ) && ! empty( $value )  && empty( $is_enable_date_range ) ) {
 			add_filter(
 				$filter_hook,
 				function ( $msg ) use ( $field_label ) {
