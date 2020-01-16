d4800258ab176e7469084de5590dbe62a7b31ae3
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-01-16 16:15:38 +0545

    restored the code for password strength

--- includes/shortcodes/class-ur-shortcode-my-account.php
+++ includes/shortcodes/class-ur-shortcode-my-account.php
@@ -202,8 +202,8 @@ class UR_Shortcode_My_Account {
 	public static function edit_account() {
 		$user_id                   = get_current_user_id();
 		$form_id                   = get_user_meta( $user_id, 'ur_form_id', true );
-		$enable_strong_password    = get_option( 'user_registration_my_account_enable_strong_password', 'yes' );
-		$minimum_password_strength = get_option( 'user_registration_my_account_minimum_password_strength', 3 );
+		$enable_strong_password    = ur_get_single_post_meta( $form_id, 'user_registration_form_setting_enable_strong_password' );
+		$minimum_password_strength = ur_get_single_post_meta( $form_id, 'user_registration_form_setting_minimum_password_strength');
 
 		if ( 'yes' === $enable_strong_password ) {
 			wp_enqueue_script( 'ur-password-strength-meter' );
