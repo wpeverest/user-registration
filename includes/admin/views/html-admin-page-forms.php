6bc17e23f90ef712fcfa7554fbbf478f9a42d3e8
Nilan Bhasima <nilanbhas@gmail.com>
2020-02-17 15:29:46 +0545

    Search field UI styles

--- includes/admin/views/html-admin-page-forms.php
+++ includes/admin/views/html-admin-page-forms.php
@@ -66,7 +66,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 								<div class="ur-tab-contents" >
 									<div id="ur-tab-registered-fields" class="ur-tab-content">
-										<input id="ur-search-fields" class="ur-type-text" type="text" placeholder="Search Fields..." />
+										<div class="ur-search-input ur-search-fields">
+											<input id="ur-search-fields" class="ur-type-text" type="text" placeholder="Search Fields..." />
+											<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24" fill="#a1a4b9"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"/></svg>
+										</div>
 										<h2 class='ur-toggle-heading'><?php echo __( 'Default User Fields', 'user-registration' ); ?></h2>
 										<hr/>
 										<?php $this->get_registered_user_form_fields(); ?>
