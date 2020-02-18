2ebad7f4ed5e09414605525e9b99e8ef6b7a8ab7
Nilan Bhasima <nilanbhas@gmail.com>
2020-02-18 11:33:39 +0545

    UX for field search result

--- includes/admin/views/html-admin-page-forms.php
+++ includes/admin/views/html-admin-page-forms.php
@@ -71,8 +71,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 											<svg xmlns="http://www.w3.org/2000/svg" height="24px" width="24px" viewBox="0 0 24 24" fill="#a1a4b9"><path d="M21.71,20.29,18,16.61A9,9,0,1,0,16.61,18l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,21.71,20.29ZM11,18a7,7,0,1,1,7-7A7,7,0,0,1,11,18Z"/></svg>
 										</div>
 										<div class="ur-fields-not-found" hidden>
-											<img src="<?php echo esc_attr( plugin_dir_url( UR_PLUGIN_FILE ) . 'assets/images/not-found.png' ) ?>" />
-											<span>No Fields Found!</span>
+											<img src="<?php echo esc_attr( plugin_dir_url( UR_PLUGIN_FILE ) . 'assets/images/not-found.png' ); ?>" />
+											<h3 class="ur-fields-not-found-title">Whoops!</h3>
+											<span>There is not any field that you were searching for.</span>
 										</div>
 										<h2 class='ur-toggle-heading'><?php echo __( 'Default User Fields', 'user-registration' ); ?></h2>
 										<hr/>
