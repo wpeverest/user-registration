ea07e6df6ae71ea0024c976c4c5141bd7b4bb381
Krishna Suwal <58679177+krishna-suwal@users.noreply.github.com>
2020-05-27 18:10:52 +0545

    Fix - Redirect url for login in my-account shortcode

--- includes/functions-ur-template.php
+++ includes/functions-ur-template.php
@@ -50,7 +50,10 @@ function ur_login_template_redirect() {
 	if ( ( has_shortcode( $post_content, 'user_registration_login' ) || has_shortcode( $post_content, 'user_registration_my_account' ) ) && is_user_logged_in() ) {
 		preg_match( '/' . get_shortcode_regex() . '/s', $post_content, $matches );
 
-		$attributes   = shortcode_parse_atts( $matches[3] );
+		// Remove all html tags.
+		$escaped_atts_string = preg_replace( '/<[\/]{0,1}[^<>]*>/', '', $matches[3] );
+
+		$attributes   = shortcode_parse_atts( $escaped_atts_string );
 		$redirect_url = isset( $attributes['redirect_url'] ) ? $attributes['redirect_url'] : '';
 		$redirect_url = trim( $redirect_url, ']' );
 		$redirect_url = trim( $redirect_url, '"' );
