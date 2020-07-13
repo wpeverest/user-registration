dd0523c29fb2ad30d4381e965ad43c8219e92e60
Prajjwal Poudel <iamprazol@gmail.com>
2020-07-13 08:25:59 +0545

    Fix - Profile picture upload button display in ajax form submission on edit profile

--- templates/myaccount/form-edit-profile.php
+++ templates/myaccount/form-edit-profile.php
@@ -69,7 +69,7 @@ do_action( 'user_registration_before_edit_profile_form' ); ?>
 							<?php
 							if ( 'yes' === get_option( 'user_registration_ajax_form_submission_on_edit_profile', 'no' ) ) {
 								?>
-						<button type="button" class="button user_registration_profile_picture_upload hide-if-no-js" style="<?php echo isset( $profile_picture_url ) ? 'display:none;' : ''; ?>" ><?php echo __( 'Upload Picture', 'user-registration-advanced-fields' ); ?></button>
+						<button type="button" class="button user_registration_profile_picture_upload hide-if-no-js" style="<?php echo ( $gravatar_image !== $image ) ? 'display:none;' : ''; ?>" ><?php echo __( 'Upload Picture', 'user-registration-advanced-fields' ); ?></button>
 						<input type="file" id="ur-profile-pic" name="profile-pic" class="profile-pic-upload" accept="image/jpeg" style="display:none" />
 								<?php
 							} else {
