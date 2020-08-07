d6a329fb49fbed13a4c7cf3e0346ca8cb0199733
Nilan Bhasima <nilanbhas@gmail.com>
2020-08-07 10:51:41 +0545

    Tweak - Design customize for shortcode

--- includes/admin/class-ur-admin-registrations-table-list.php
+++ includes/admin/class-ur-admin-registrations-table-list.php
@@ -188,15 +188,12 @@ class UR_Admin_Registrations_Table_List extends WP_List_Table {
 	 * @return void
 	 */
 	public function column_shortcode( $registration ) {
-
 		$shortcode = '[user_registration_form id="' . $registration->ID . '"]';
-		echo sprintf( '<span class="shortcode"><input type="text" onfocus="this.select();" readonly="readonly" value=\'%s\' class=" code" size="52"></span>', $shortcode );
+		echo sprintf( '<input type="text" onfocus="this.select();" readonly="readonly" value=\'%s\' class="widefat code"></span>', $shortcode );
 		?>
-		<button id="copy-shortcode" class="button button-primary button-large ur-copy-shortcode " href="#" data-tip="<?php esc_attr_e( 'Copy Shortcode ! ', 'user - registration' ); ?>" data-copied="<?php esc_attr_e( 'Copied ! ', 'user - registration' ); ?>">
-		<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16">
-			<path fill-rule="evenodd" d="M2 13h4v1H2v-1zm5-6H2v1h5V7zm2 3V8l-3 3 3 3v-2h5v-2H9zM4.5 9H2v1h2.5V9zM2 12h2.5v-1H2v1zm9 1h1v2c-.02.28-.11.52-.3.7-.19.18-.42.28-.7.3H1c-.55 0-1-.45-1-1V4c0-.55.45-1 1-1h3c0-1.11.89-2 2-2 1.11 0 2 .89 2 2h3c.55 0 1 .45 1 1v5h-1V6H1v9h10v-2zM2 5h8c0-.55-.45-1-1-1H8c-.55 0-1-.45-1-1s-.45-1-1-1-1 .45-1 1-.45 1-1 1H3c-.55 0-1 .45-1 1z"/>
-		</svg>
-	</button>
+		<button id="copy-shortcode" class="button ur-copy-shortcode " href="#" data-tip="<?php esc_attr_e( 'Copy Shortcode ! ', 'user - registration' ); ?>" data-copied="<?php esc_attr_e( 'Copied ! ', 'user - registration' ); ?>">
+			<span class="dashicons dashicons-admin-page"></span>
+		</button>
 		<?php
 
 	}
