b25692a19db552079010824d05e67ef9131c4a51
Prajjwal Poudel <iamprazol@gmail.com>
2020-08-11 11:22:18 +0545

    Fix - My account endpoints url in home page

--- includes/functions-ur-account.php
+++ includes/functions-ur-account.php
@@ -78,21 +78,7 @@ function ur_lostpassword_url( $default_url = '' ) {
 		return $default_url;
 	}
 
-	/**
-	 * Check if the page sent as parameter is My Account page and return the id,
-	 * Else use the page's page_id sent as parameter.
-	 */
-	$my_account_page_id = get_option( 'user_registration_myaccount_page_id' );
-	$page_id            = get_the_ID();
-	if ( ur_post_content_has_shortcode( 'user_registration_my_account' ) && $page_id === $my_account_page_id ) {
-		$page = $page_id;
-	} else {
-		$page = apply_filters( 'user_registration_get_myaccount_page_id', get_option( 'user_registration_myaccount_page_id' ) );
-	}
-
-	$permalink = 0 < $page ? get_permalink( $page ) : get_home_url();
-
-	$ur_account_page_url = apply_filters( 'user_registration_get_' . $page . '_page_permalink', $permalink );
+	$ur_account_page_url = ur_get_page_permalink( 'myaccount' );
 
 	$ur_account_page_exists = ur_get_page_id( 'myaccount' ) > 0;
 	$lost_password_endpoint = get_option( 'user_registration_myaccount_lost_password_endpoint', 'lost-password' );
