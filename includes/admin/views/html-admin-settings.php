d2820ff3ec9db62019610d900a233272e6850fed
Nilan Bhasima <nilanbhas@gmail.com>
2020-11-27 14:12:16 +0545

    Tweak - Setting page header UI

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -10,19 +10,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="wrap user-registration">
 	<form method="<?php echo esc_attr( apply_filters( 'user_registration_settings_form_method_tab_' . $current_tab, 'post' ) ); ?>" id="mainform" action="" enctype="multipart/form-data">
-		<nav class="nav-tab-wrapper ur-nav-tab-wrapper">
-			<?php
-			foreach ( $tabs as $name => $label ) {
-				echo '<a href="' . admin_url( 'admin.php?page=user-registration-settings&tab=' . $name ) . '" class="nav-tab ' . ( $current_tab == $name ? 'nav-tab-active' : '' ) . '">' . $label . '</a>';
-			}
+		<header class="user-registration-header">
+			<nav class="nav-tab-wrapper ur-nav ur-nav--tab ur-nav-tab-wrapper">
+				<?php
+				foreach ( $tabs as $name => $label ) {
+					echo '<a href="' . admin_url( 'admin.php?page=user-registration-settings&tab=' . $name ) . '" class="nav-tab ur-nav__link ' . ( $current_tab == $name ? 'nav-tab-active is-active' : '' ) . '">' . $label . '</a>';
+				}
 
-				do_action( 'user_registration_settings_tabs' );
+					do_action( 'user_registration_settings_tabs' );
+				?>
+			</nav>
+			<?php
+				do_action( 'user_registration_sections_' . $current_tab );
 			?>
-		</nav>
+		</header>
 		<h1 class="screen-reader-text"><?php echo esc_html( $tabs[ $current_tab ] ); ?></h1>
 		<?php
-			do_action( 'user_registration_sections_' . $current_tab );
-
 			self::show_messages();
 
 			do_action( 'user_registration_settings_' . $current_tab );
