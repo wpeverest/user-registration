678d9fa22de48230bdaa6dbcfd29c179fce95884
Lekhnath Pandey <lekhnath005@gmail.com>
2021-03-26 08:32:19 +0545

    Tweak - Handle registered email sending after ideal payment completion

--- includes/frontend/class-ur-frontend-form-handler.php
+++ includes/frontend/class-ur-frontend-form-handler.php
@@ -116,7 +116,10 @@ class UR_Frontend_Form_Handler {
 				$success_params['form_login_option'] = $login_option;
 				$success_params                      = apply_filters( 'user_registration_success_params', $success_params, self::$valid_form_data, $form_id, $user_id );
 
-				do_action( 'user_registration_after_register_user_action', self::$valid_form_data, $form_id, $user_id );
+				if ( ! isset( $_POST['ur_stripe_payment_method'] ) || 'ideal' !==  sanitize_text_field( $_POST['ur_stripe_payment_method'] ) ) {
+
+					do_action( 'user_registration_after_register_user_action', self::$valid_form_data, $form_id, $user_id );
+				}
 				wp_send_json_success( $success_params );
 			}
 			wp_send_json_error(
