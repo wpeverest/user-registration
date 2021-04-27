7c776131d300d2a4d5a9c3b8fa49b16cb02aee6b
dipuchaudhary <dipuchaudhary8@gmail.com>
2021-04-27 18:09:05 +0545

    Fix - Profile picture error when ajax submission enabled

--- templates/myaccount/dashboard.php
+++ templates/myaccount/dashboard.php
@@ -39,12 +39,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 			$profile_picture_url = get_user_meta( get_current_user_id(), 'user_registration_profile_pic_url', true );
 			$image               = ( ! empty( $profile_picture_url ) ) ? $profile_picture_url : $gravatar_image;
 
-			if( 'no' === get_option( 'user_registration_disable_profile_picture', 'yes' ) ) {
-				
+			if( 'no' === get_option( 'user_registration_disable_profile_picture', 'no' ) ) {
+
 			?>
 				<img class="profile-preview" alt="profile-picture" src="<?php echo $image; ?>">
 			<?php } ?>
-		
+
 	</div>
 	<header>
 		<?php
