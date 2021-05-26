685cea458a79c1797e1e924f9dcb8ab3c1725504
Deependra Chaudhary <elendoff11@gmail.com>
2021-05-26 15:08:40 +0545

    Enhance - Reset Button in UR Form (#335)
    * Added - Hook for reset button
    
    * Code Cleanup
    
    * Code cleanup
    
    * Code cleanup
    
    Co-authored-by: Deependra Chaudhary <deependra.we@gmail.com>

--- templates/form-registration.php
+++ templates/form-registration.php
@@ -134,7 +134,6 @@ do_action( 'user_registration_before_registration_form', $form_id );
 						$submit_btn_class = apply_filters( 'user_registration_form_submit_btn_class', array(), $form_id );
 						$submit_btn_class = array_merge( $submit_btn_class, (array) ur_get_form_setting_by_key( $form_id, 'user_registration_form_setting_form_submit_class' ) );
 						?>
-
 						<button type="submit" class="btn button ur-submit-button <?php echo esc_attr( implode( ' ', $submit_btn_class ) ); ?>">
 							<span></span>
 							<?php
@@ -142,8 +141,8 @@ do_action( 'user_registration_before_registration_form', $form_id );
 								echo ur_string_translation( $form_id, 'user_registration_form_setting_form_submit_label', $submit );
 							?>
 						</button>
-
 						<?php do_action( 'user_registration_after_form_buttons', $form_id ); ?>
+						<?php do_action( 'user_registration_after_submit_buttons', $form_id ); ?>
 					</div>
 					<?php
 			}
