f7c531240ca0dbe73c080aab7f7b276db533dfed
Deependra Chaudhary <deependra.we@gmail.com>
2021-06-14 09:43:10 +0545

    Added - Enable/disable for UR form field icon

--- includes/functions-ur-template.php
+++ includes/functions-ur-template.php
@@ -253,9 +253,10 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 		$label_id        = $args['id'];
 		$sort            = $args['priority'] ? $args['priority'] : '';
 		$field_container = '<div class="form-row %1$s" id="%2$s" data-priority="' . esc_attr( $sort ) . '" ' . $cl_html . '>%3$s</div>';
-		$user_id = get_current_user_id();
-		$form_id = ur_get_form_id_by_userid( $user_id );
-		$enable_field_icon   = ur_get_single_post_meta( $form_id, 'user_registration_form_setting_enable_field_icon' );
+		if(	!is_admin(  ) ) {
+		$form_id = $args['form_id'];
+		$enable_field_icon   = ur_get_single_post_meta( $form_id, 'user_registration_enable_field_icon' );
+		}
 
 		switch ( $args['type'] ) {
 
@@ -332,8 +333,8 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 				} else {
 					$field .= '<input data-rules="' . esc_attr( $rules ) . '" data-id="' . esc_attr( $key ) . '" type="' . esc_attr( $args['type'] ) . '" class="input-text ' . esc_attr( implode( ' ', $args['input_class'] ) ) . '" name="' . esc_attr( $key ) . '" id="' . esc_attr( $args['id'] ) . '" placeholder="' . esc_attr( $args['placeholder'] ) . '"  value="' . esc_attr( $value ) . '" ' . implode( ' ', $custom_attributes ) . ' />';
 				}
-				if('yes' === $enable_field_icon || '1'===$enable_field_icon) {
 					if(!is_admin(  )){
+						if('yes' === $enable_field_icon || '1'===$enable_field_icon) {
 						$field .= '<span class="'.$args['icon'].'"></span>';
 					}
 				}
@@ -358,20 +359,18 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 				if( $username_character ) {
 					$attr .= 'data-username-character="' . $username_character . '"';
 				}
-				$field .= '<span class="input-form-field-icons">';
 				if ( empty( $extra_params ) ) {
 					$field .= '<input data-rules="' . esc_attr( $rules ) . '" data-id="' . esc_attr( $key ) . '" type="' . esc_attr( $args['type'] ) . '" class="input-text input-' . esc_attr( $args['type'] ) . ' ' . esc_attr( implode( ' ', $args['input_class'] ) ) . '" name="' . esc_attr( $key ) . '" id="' . esc_attr( $args['id'] ) . '" placeholder="' . esc_attr( $args['placeholder'] ) . '"  value="' . esc_attr( $value ) . '" ' . implode( ' ', $custom_attributes ) . ' ' . $attr . '/>';
 				} else {
 					$field .= '<input data-rules="' . esc_attr( $rules ) . '" data-id="' . esc_attr( $key ) . '" type="' . esc_attr( $args['type'] ) . '" class="input-text ' . esc_attr( implode( ' ', $args['input_class'] ) ) . '" name="' . esc_attr( $key ) . '" id="' . esc_attr( $args['id'] ) . '" placeholder="' . esc_attr( $args['placeholder'] ) . '"  value="' . esc_attr( $value ) . '" ' . implode( ' ', $custom_attributes ) . ' ' . $attr . ' />';
 				}
-				if('yes' === $enable_field_icon || '1'===$enable_field_icon) {
 					if(!is_admin(  ) && 'file' != $args['type']){
-						$field .= '<span class="'.$args['icon'].'"></span>';
+						if('yes' === $enable_field_icon || '1'===$enable_field_icon) {
+							$field .= '<span class="'.$args['icon'].'"></span>';
 					}
 				}
-				$field .= '</span>';
-
 				break;
+
 			case 'date':
 				$extra_params_key = str_replace( 'user_registration_', 'ur_', $key ) . '_params';
 				$extra_params     = json_decode( get_user_meta( get_current_user_id(), $extra_params_key, true ) );
@@ -402,8 +401,9 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 					$field .= '<input type="hidden" id="formated_date" value="' . esc_attr( $value ) . '"/>';
 					$field .= '<input data-rules="' . esc_attr( $rules ) . '" data-id="' . esc_attr( $key ) . '" type="text" data-field-type="' . esc_attr( $args['type'] ) . '" value="' . esc_attr( $actual_value ) . '" class="input-text ' . esc_attr( implode( ' ', $args['input_class'] ) ) . '" name="' . esc_attr( $key ) . '" id="' . esc_attr( $args['id'] ) . '" placeholder="' . esc_attr( $args['placeholder'] ) . '"  ' . implode( ' ', $custom_attributes ) . ' style="display:none" />';
 				}
-				if('yes' === $enable_field_icon || '1'===$enable_field_icon) {
+
 					if(!is_admin(  ) ){
+						if('yes' === $enable_field_icon || '1'===$enable_field_icon) {
 						$field .= '<span class="'.$args['icon'].'"></span>';
 					}
 				}
