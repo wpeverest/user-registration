b7a12af58685e253cb7b382edca08e4055ccb36d
Prajjwal Poudel <iamprazol@gmail.com>
2021-09-28 14:41:46 +0545

    Resolve free and pro version activation conflicts

--- user-registration.php
+++ user-registration.php
@@ -325,16 +325,111 @@ if ( ! class_exists( 'UserRegistration' ) ) :
 endif;
 
 /**
- * Main instance of UserRegistration.
+ * Check to see if UR already defined and resolve conflicts while installing PRO version.
  *
- * Returns the main instance of FT to prevent the need to use globals.
- *
- * @since  1.0.0
- * @return UserRegistration
+ * @since 2.0.4
  */
-function UR() {
-	return UserRegistration::instance();
-}
 
+if ( ! function_exists( 'UR' ) ) {
+
+	/**
+	 * Main instance of UserRegistration.
+	 *
+	 * Returns the main instance of FT to prevent the need to use globals.
+	 *
+	 * @since  1.0.0
+	 * @return UserRegistration
+	 */
+	function UR() {
+		return UserRegistration::instance();
+	}
+
+} else {
+
+	if ( ! function_exists( 'user_registration_pro_activated' ) ) {
+		/**
+		 * When Pro version is activated, deactivate free version.
+		 */
+		function user_registration_pro_activated() {
+
+			user_registration_free_deactivate();
+
+		}
+	}
+	add_action( 'activate_user-registration-pro/user-registration.php', 'user_registration_pro_activated' );
+
+	if ( ! function_exists( 'user_registration_free_activated' ) ) {
+		/**
+		 * When user activates free version, set the value that is to be used to handle both Free and Pro activation conflict.
+		 */
+		function user_registration_free_activated() {
+
+			set_transient( 'user_registration_free_activated', true );
+		}
+	}
+	add_action( 'activate_user-registration/user-registration.php', 'user_registration_free_activated' );
+
+	if ( ! function_exists( 'user_registration_free_deactivated' ) ) {
+		/**
+		 * When user deactivates free version, remove the value that was used to handle both Free and Pro activation conflict.
+		 */
+		function user_registration_free_deactivated() {
+
+			global $user_registration_free_activated, $user_registration_free_deactivated;
+
+			$user_registration_free_activated   = (bool) get_transient( 'user_registration_free_activated' );
+			$user_registration_free_deactivated = true;
+
+			delete_transient( 'user_registration_free_activated' );
+		}
+	}
+	add_action( 'deactivate_user-registration/user-registration.php', 'user_registration_free_deactivated' );
+
+	if ( ! function_exists( 'user_registration_free_deactivate' ) ) {
+		/**
+		 * Deactivate Free version if Pro is already activated.
+		 *
+		 * @since 1.0.0
+		 */
+		function user_registration_free_deactivate() {
+
+			$plugin = 'user-registration/user-registration.php';
+
+			deactivate_plugins( $plugin );
+
+			do_action( 'user_registration_free_deactivate', $plugin );
+		}
+	}
+	add_action( 'admin_init', 'user_registration_free_deactivate' );
+
+	if ( ! function_exists( 'user_registration_free_notice' ) ) {
+		/**
+		 * When user wants to activate Free version alongside Pro, then display the message.
+		 */
+		function user_registration_free_notice() {
+
+			global $user_registration_free_activated, $user_registration_free_deactivated;
+
+			if (
+				empty( $user_registration_free_activated ) ||
+				empty( $user_registration_free_deactivated )
+			) {
+				return;
+			}
+
+			echo '<div class="notice-warning notice is-dismissible"><p>' . esc_html__( 'User Registration Pro is already activated. If you want to install User Registration Free please deactivate User Registration Pro', 'user-registration' ) . '</p></div>';
+
+			if ( isset( $_GET['activate'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+				unset( $_GET['activate'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			}
+
+			unset( $user_registration_free_activated, $user_registration_free_deactivated );
+		}
+	}
+	add_action( 'admin_notices', 'user_registration_free_notice' );
+
+	// Do not process the plugin code further.
+	return;
+}
 // Global for backwards compatibility.
 $GLOBALS['user-registration'] = UR();
