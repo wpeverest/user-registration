ecf5093cbb1a382493ff8fc30453c862ad0cf610
Prajjwal Poudel <85762481+iamprazol@users.noreply.github.com>
2022-01-06 13:29:28 +0545

    UR-91 Enhance - Extension page design and addons installation. (#382)
    * Tweak - Extension installation from addons page support
    
    * Tweak - Extension page design change
    
    * Tweak - PRO install method
    
    * Tweak - Missing images added
    
    * Tweak - Optimized images added
    
    * Merge branch 'develop' into enhance/extension-install
    
    * Tweak - Refresh addons transient for once

--- includes/admin/views/html-admin-page-addons.php
+++ includes/admin/views/html-admin-page-addons.php
@@ -29,22 +29,43 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php endforeach; ?>
 		</ul>
 		<br class="clear" />
-		<?php $addons = UR_Admin_Addons::get_section_data( $current_section ); ?>
-			<?php if ( $addons ) : ?>
-			<ul class="products">
-				<?php foreach ( $addons as $addon ) : ?>
-				<li class="product">
-					<a href="<?php echo esc_attr( $addon->link ); ?>">
-						<h2><?php echo esc_html( $addon->title ); ?></h2>
-						<?php if ( ! empty( $addon->image ) ) : ?>
-							<span class="product-image"><img src="<?php echo esc_url( UR()->plugin_url() . '/assets/' . $addon->image ); ?>"/></span>
-						<?php endif; ?>
-						<span class="price"><?php echo isset( $addon->price ) ? wp_kses_post( $addon->price ) : ''; ?></span>
-						<p><?php echo wp_kses_post( $addon->excerpt ); ?></p>
-					</a>
-				</li>
-			<?php endforeach; ?>
-			</ul>
+		<?php if ( $addons = UR_Admin_Addons::get_section_data( $current_section ) ) : ?>
+			<div class="wp-list-table widefat extension-install">
+					<h2 class="screen-reader-text"><?php esc_html_e( 'Extensions list', 'user-registration' ); ?></h2>
+
+					<div class="the-list">
+						<?php foreach ( $addons as $addon ) : ?>
+							<div class="plugin-card plugin-card-<?php echo esc_attr( $addon->slug ); ?>">
+								<div class="plugin-card-left">
+									<a href="<?php echo esc_url( $addon->link ); ?>">
+										<img src="<?php echo esc_url( UR()->plugin_url() . '/assets/' . $addon->image ); ?>" class="plugin-icon" alt="" />
+									</a>
+								</div>
+								<div class="plugin-card-right">
+									<div class="name column-name">
+										<a href="<?php echo esc_url( $addon->link ); ?>">
+											<h3 class="plugin-name">
+												<?php echo esc_html( $addon->title ); ?>
+											</h3>
+										</a>
+									</div>
+									<div class="desc column-description">
+										<p class="plugin-desc"><?php echo esc_html( $addon->excerpt ); ?></p>
+									</div>
+									<div class="plugin-card-buttons">
+										<?php if ( get_option( 'user-registration_license_key' ) ) { ?>
+												<?php echo do_action( 'user_registration_after_addons_description', $addon ); ?>
+											<?php } else { ?>
+												<div class="action-buttons upgrade-plan">
+													<a class="button upgrade-now" href="https://wpeverest.com/wordpress-plugins/user-registration/pricing/?utm_source=addons-page&utm_medium=upgrade-button&utm_campaign=evf-upgrade-to-pro" target="_blank"><?php esc_html_e( 'Upgrade Plan', 'user-registration' ); ?></a>
+												</div>
+										<?php } ?>
+									</div>
+								</div>
+							</div>
+						<?php endforeach; ?>
+					</div>
+				</div>
 		<?php endif; ?>
 	<?php else : ?>
 		<p>
