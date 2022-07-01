097cb46675f80d1a0929bcaa62f818fb53fe9169
Prajjwal Poudel <iamprazol@gmail.com>
2022-07-01 07:17:06 +0545

    Fix - Phpcs

--- templates/myaccount/form-edit-profile.php
+++ templates/myaccount/form-edit-profile.php
@@ -35,7 +35,7 @@ do_action( 'user_registration_before_edit_profile_form' ); ?>
 								<?php
 								$gravatar_image      = get_avatar_url( get_current_user_id(), $args = null );
 								$profile_picture_url = get_user_meta( get_current_user_id(), 'user_registration_profile_pic_url', true );
-								
+
 								if ( is_numeric( $profile_picture_url ) ) {
 									$profile_picture_url  = wp_get_attachment_url( $profile_picture_url );
 								}
@@ -75,7 +75,7 @@ do_action( 'user_registration_before_edit_profile_form' ); ?>
 												<p class="form-row " id="profile_pic_url_field" data-priority="">
 													<span class="uraf-profile-picture-upload-node" style="height: 0;width: 0;margin: 0;padding: 0;float: left;border: 0;overflow: hidden;">
 													<input type="file" id="ur-profile-pic" name="profile-pic" class="profile-pic-upload" size="<?php echo esc_attr( $max_upload_size ); ?>" accept="<?php echo esc_attr( $edit_profile_valid_file_type ); ?>" style="<?php echo esc_attr( ( $gravatar_image !== $image ) ? 'display:none;' : '' ); ?>" />
-													<?php echo '<input type="text" class="uraf-profile-picture-input input-text ur-frontend-field" name="profile_pic_url" id="profile_pic_url" value="' . get_user_meta( get_current_user_id(), 'user_registration_profile_pic_url', true ) . '" />'; ?>
+													<?php echo '<input type="text" class="uraf-profile-picture-input input-text ur-frontend-field" name="profile_pic_url" id="profile_pic_url" value="' . get_user_meta( get_current_user_id(), 'user_registration_profile_pic_url', true ) . '" />'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 													</span>
 													<?php do_action( 'uraf_profile_picture_buttons' ); ?>
 												</p>
