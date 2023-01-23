41e886d96d6435a658fdb6e6a463bf89caac2d9c
Salim Shrestha <26961315+salimshrestha98@users.noreply.github.com>
2023-01-23 14:25:15 +0545

    Fix - Remove common validations from field class

--- includes/form/class-ur-form-field-number.php
+++ includes/form/class-ur-form-field-number.php
@@ -70,21 +70,7 @@ class UR_Form_Field_Number extends UR_Form_Field {
 	 * @param [int]    $form_id Form id.
 	 */
 	public function validation( $single_form_field, $form_data, $filter_hook, $form_id ) {
-		$required             = isset( $single_form_field->general_setting->required ) ? $single_form_field->general_setting->required : 'no';
-		$field_label          = isset( $form_data->label ) ? $form_data->label : '';
-		$value                = isset( $form_data->value ) ? $form_data->value : '';
-		$urcl_hide_fields = isset( $_POST['urcl_hide_fields'] ) ? (array) json_decode( stripslashes( $_POST['urcl_hide_fields'] ), true ) : array(); //phpcs:ignore;
-		$field_name       = isset( $single_form_field->general_setting->field_name ) ? $single_form_field->general_setting->field_name : '';
-
-		if ( ! in_array( $field_name, $urcl_hide_fields, true ) && 'yes' == $required && empty( $value ) ) {
-			add_filter(
-				$filter_hook,
-				function ( $msg ) use ( $field_label ) {
-					/* translators: %1$s - Field Label */
-					return sprintf( __( '%1$s is required.', 'user-registration' ), $field_label );
-				}
-			);
-		}
+		// Perform custom validation for the field here ...
 
 		if ( ! is_numeric( $value ) ) {
 			add_filter(
