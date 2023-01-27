de9cd461c44c3682269fe12d2c9b43d030c44fb3
dipuchaudhary8 <dipuchaudhary8@gmail.com>
2023-01-27 14:53:42 +0545

    Fix - Two error message appearing at same time using transient

--- includes/shortcodes/class-ur-shortcode-login.php
+++ includes/shortcodes/class-ur-shortcode-login.php
@@ -40,8 +40,10 @@ class UR_Shortcode_Login {
 
 		if ( ! is_user_logged_in() ) {
 			// After password reset, add confirmation message.
-			if ( ! empty( $_GET['password-reset'] ) ) {
+			$is_password_resetted = get_transient( 'ur_password_resetted_flag' );
+			if ( ! empty( $is_password_resetted ) ) {
 				ur_add_notice( __( 'Your password has been reset successfully.', 'user-registration' ) );
+				delete_transient( 'ur_password_resetted_flag' );
 			}
 			if ( isset( $wp->query_vars['ur-lost-password'] ) ) {
 				UR_Shortcode_My_Account::lost_password();
