2cc698c39fc62d9845e9060a5ad2b2b536877c37
Umesh Ghimire <me.umeshuser@gmail.com>
2023-02-07 11:08:40 +0545

    deactivation feedback implemented

--- assets/js/admin/deactivation-feedback.js
+++ assets/js/admin/deactivation-feedback.js
@@ -6,56 +6,93 @@ jQuery(function ($) {
 		},
 		event_init: function () {
 			var _that = this;
+
 			$(document.body).on(
 				"click",
 				'tr[data-plugin="user-registration/user-registration.php"] span.deactivate a',
 				function (e) {
 					e.preventDefault();
-					$('#ur-deactivate-feedback-popup-wrapper').addClass('active');
-				});
-			$('#ur-deactivate-feedback-popup-wrapper').click(function (event) {
+					$("#ur-deactivate-feedback-popup-wrapper").addClass(
+						"active"
+					);
+				}
+			);
+
+			$(document.body).on(
+				"click",
+				'tr[data-plugin="user-registration-pro/user-registration.php"] span.deactivate a',
+				function (e) {
+					e.preventDefault();
+					$("#ur-deactivate-feedback-popup-wrapper").addClass(
+						"active"
+					);
+				}
+			);
+
+			$("#ur-deactivate-feedback-popup-wrapper").click(function (event) {
 				var $target = $(event.target);
-				if (!$target.closest('.ur-deactivate-feedback-popup-inner').length) {
-					$('#ur-deactivate-feedback-popup-wrapper').removeClass('active');
+				if (
+					!$target.closest(".ur-deactivate-feedback-popup-inner")
+						.length
+				) {
+					$("#ur-deactivate-feedback-popup-wrapper").removeClass(
+						"active"
+					);
 				}
 			});
-			$('form.ur-deactivate-feedback-form').on('submit', function (e) {
+
+			$("form.ur-deactivate-feedback-form").on("submit", function (e) {
 				e.preventDefault();
 				_that.send_data($(this));
 			});
+
+			$('#ur-deactivate-feedback-popup-wrapper').on('click', '.close-deactivate-feedback-popup', function(){
+				$('#ur-deactivate-feedback-popup-wrapper').removeClass('active');
+			});
 		},
 		send_data: function (form) {
-			var reason_slug = form.find('input[name="reason_slug"]:checked').val();
+			var reason_slug = form
+				.find('input[name="reason_slug"]:checked')
+				.val();
+
 			if (reason_slug === undefined) {
-				alert('Please select at least one option from the list');
+				alert("Please select at least one option from the list");
 				return;
 			}
-			if (form.find('button.submit').hasClass('button-disabled')) {
+
+			if (form.find("button.submit").hasClass("button-disabled")) {
 				return;
 			}
-			var reason_text = '';
-			var reason_text_el = form.find('input[name="reason_' + reason_slug + '"]');
+
+			var reason_text = "";
+			var reason_text_el = form.find(
+				'input[name="reason_' + reason_slug + '"]'
+			);
+
 			if (reason_text_el.length > 0) {
 				reason_text = reason_text_el.val();
 			}
+
 			var data = {
 				reason_slug: "user_registration_deactivation_notice",
 			};
-			data[('reason_' + reason_slug)] = reason_text;
+
+			data["reason_" + reason_slug] = reason_text;
+
 			$.ajax({
 				url: ur_plugins_params.ajax_url,
 				data: form.serializeArray(),
 				type: "post",
 				beforeSend: function () {
-					form.find('button.submit').addClass('button-disabled button updating-message');
+					form.find("button.submit").addClass(
+						"button-disabled button updating-message"
+					);
 				},
-
-			}).done(function () { //use this
-				window.location = form.find('a.skip').attr('href');
+			}).done(function () {
+				window.location = form.find("a.skip").attr("href");
 			});
-		}
+		},
 	};
 
 	ur_deactivation_feedback.init();
-
 });
--- includes/admin/views/html-deactivation-popup.php
+++ includes/admin/views/html-deactivation-popup.php
@@ -19,38 +19,44 @@ $deactivate_url = wp_nonce_url( 'plugins.php?action=deactivate&amp;plugin=' . UR
 <div id="ur-deactivate-feedback-popup-wrapper">
 	<div class="ur-deactivate-feedback-popup-inner">
 		<div class="ur-deactivate-feedback-popup-header">
-				<span class="ur-deactivate-feedback-popup-header-title"><?php echo esc_html__('Quick Feedback', 'user-registration'); ?></span>
+			<div class="ur-deactivate-feedback-popup-header__logo-wrap">
+				<div class="ur-deactivate-feedback-popup-header__logo-icon">
+					<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style>.cls-1{fill:#475bb2;}</style></defs><title>Artboard 1 copy 3</title><path class="cls-1" d="M27.58,4a27.9,27.9,0,0,0-5.17,4,27,27,0,0,0-4.09,5.08,33.06,33.06,0,0,1,2,4.65A23.78,23.78,0,0,1,24,12.15V18a8,8,0,0,1-5.89,7.72l-.21.05A27,27,0,0,0,16,17.61,27.9,27.9,0,0,0,9.59,8,27.9,27.9,0,0,0,4.42,4L4,3.77V18a12,12,0,0,0,9.93,11.82l.14,0a11.72,11.72,0,0,0,3.86,0l.14,0A12,12,0,0,0,28,18V3.77ZM8,18V12.15a23.86,23.86,0,0,1,5.89,13.57A8,8,0,0,1,8,18ZM16,2a3,3,0,1,0,3,3A3,3,0,0,0,16,2Z"/></svg>
+				</div>
+				<span class="ur-deactivate-feedback-popup-header-title"><?php echo esc_html__( 'Quick Feedback', 'user-registration' ); ?></span>
+			</div>
+			<a class="close-deactivate-feedback-popup"><span class="dashicons dashicons-no-alt"></span></a>
 		</div>
 		<form class="ur-deactivate-feedback-form" method="POST">
 			<?php
-			wp_nonce_field('_ur_deactivate_feedback_nonce');
+			wp_nonce_field( '_ur_deactivate_feedback_nonce' );
 			?>
 			<input type="hidden" name="action" value="ur_deactivate_feedback"/>
 
 			<div
-				class="ur-deactivate-feedback-popup-form-caption"><?php echo sprintf(esc_html__('Could you please share why you are deactivating %sUser Registration%s plugin?', 'user-registration'), '<span>', '</span>'); ?></div>
+				class="ur-deactivate-feedback-popup-form-caption"><?php echo sprintf( esc_html__( 'Could you please share why you are deactivating %1$sUser Registration%2$s plugin?', 'user-registration' ), '<span>', '</span>' ); ?></div>
 			<div class="ur-deactivate-feedback-popup-form-body">
-				<?php foreach ($deactivate_reasons as $reason_slug => $reason) : ?>
+				<?php foreach ( $deactivate_reasons as $reason_slug => $reason ) : ?>
 					<div class="ur-deactivate-feedback-popup-input-wrapper">
-						<input id="ur-deactivate-feedback-<?php echo esc_attr($reason_slug); ?>"
+						<input id="ur-deactivate-feedback-<?php echo esc_attr( $reason_slug ); ?>"
 							   class="ur-deactivate-feedback-input" type="radio" name="reason_slug"
-							   value="<?php echo esc_attr($reason_slug); ?>"/>
-						<label for="ur-deactivate-feedback-<?php echo esc_attr($reason_slug); ?>"
-							   class="ur-deactivate-feedback-label"><?php echo esc_html($reason['title']); ?></label>
-						<?php if (!empty($reason['input_placeholder'])) : ?>
+							   value="<?php echo esc_attr( $reason_slug ); ?>"/>
+						<label for="ur-deactivate-feedback-<?php echo esc_attr( $reason_slug ); ?>"
+							   class="ur-deactivate-feedback-label"><?php echo esc_html( $reason['title'] ); ?></label>
+						<?php if ( ! empty( $reason['input_placeholder'] ) ) : ?>
 							<input class="ur-feedback-text" type="text"
-								   name="reason_<?php echo esc_attr($reason_slug); ?>"
-								   placeholder="<?php echo esc_attr($reason['input_placeholder']); ?>"/>
+								   name="reason_<?php echo esc_attr( $reason_slug ); ?>"
+								   placeholder="<?php echo esc_attr( $reason['input_placeholder'] ); ?>"/>
 						<?php endif; ?>
 					</div>
 				<?php endforeach; ?>
 			</div>
 			<div class="ur-deactivate-feedback-popup-form-footer">
-				<button class="submit" type="submit"><?php esc_html_e('Submit &amp; Deactivate', 'user-registration'); ?>
-				</button>
-				<a href="<?php echo esc_url($deactivate_url) ?>" class="skip"><?php esc_html_e('Skip &amp; Deactivate', 'user-registration'); ?></a>
+				<a href="<?php echo esc_url( $deactivate_url ); ?>" class="skip"><?php esc_html_e( 'Skip &amp; Deactivate', 'user-registration' ); ?></a>
+				<button class="submit" type="submit"><?php esc_html_e( 'Submit &amp; Deactivate', 'user-registration' ); ?></button>
 			</div>
-			<span class="consent">* <?php esc_html_e('By submitting this form, you will also be sending us your email address & website URL.', 'user-registration'); ?></span>
+			<span class="consent">* <?php esc_html_e( 'By submitting this form, you will also be sending us your email address & website URL.', 'user-registration' ); ?></span>
 		</form>
 	</div>
 </div>
+
