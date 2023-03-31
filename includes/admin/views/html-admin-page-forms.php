42ae06ea0e0e57d1b9602e1734b3cb41e1d5dd26
Ayush Shrestha <ayushstha143@gmail.com>
2023-03-31 10:40:24 +0545

    collapse button moved inside the sidebar wrapper

--- includes/admin/views/html-admin-page-forms.php
+++ includes/admin/views/html-admin-page-forms.php
@@ -110,12 +110,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 									<?php do_action( 'user_registration_form_bulder_content', $form_id ); ?>
 								</div>
 							</nav>
-						</div>
-						<button id="ur-collapse" class="close">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-								<path fill="#6B6B6B" d="M16.5 22a1.003 1.003 0 0 1-.71-.29l-9-9a1 1 0 0 1 0-1.42l9-9a1.004 1.004 0 1 1 1.42 1.42L8.91 12l8.3 8.29A.999.999 0 0 1 16.5 22Z"/>
-							</svg>
-						</button>
+							<button id="ur-collapse" class="close">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+									<path fill="#6B6B6B" d="M16.5 22a1.003 1.003 0 0 1-.71-.29l-9-9a1 1 0 0 1 0-1.42l9-9a1.004 1.004 0 1 1 1.42 1.42L8.91 12l8.3 8.29A.999.999 0 0 1 16.5 22Z"/>
+								</svg>
+							</button>
+						</div>						
 						<?php
 						$builder_class = apply_filters( 'user_registration_builder_class', array() );
 						$builder_class = implode( ' ', $builder_class );
