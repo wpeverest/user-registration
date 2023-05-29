d86516273caf3e87ed3e60a9203d54a1599a0199
deepench <elendoff11@gmail.com>
2023-05-29 10:09:17 +0545

    Added - Content restriction screen id

--- includes/admin/functions-ur-admin.php
+++ includes/admin/functions-ur-admin.php
@@ -126,6 +126,7 @@ function ur_get_screen_ids() {
 		$ur_screen_id . '_page_user-registration-addons',
 		$ur_screen_id . '_page_user-registration-export-users',
 		$ur_screen_id . '_page_user-registration-email-templates',
+		$ur_screen_id . '_page_user-registration-content-restriction',
 		'profile',
 		'user-edit',
 	);
