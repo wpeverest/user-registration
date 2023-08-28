ba1bafb2f7a07c7a65fcf3108a6410ad3afe960a
Milan Chaudhary <chaudharymilan996@gmail.com>
2023-08-28 14:52:12 +0545

    Added - Default active tab, notices area, copy icon

--- includes/admin/class-ur-admin-registrations-table-list.php
+++ includes/admin/class-ur-admin-registrations-table-list.php
@@ -159,13 +159,14 @@ class UR_Admin_Registrations_Table_List extends UR_List_Table {
 		?>
 			<div class="wrap">
 				<h1 class="wp-heading-inline"><?php esc_html_e( 'User Registration' ); ?></h1>
+				<a href="<?php echo esc_url( admin_url( 'admin.php?page=add-new-registration' ) ); ?>" class="page-title-action"><?php esc_html_e( 'Add New', 'user-registration' ); ?></a>
 				<div class="user-registration-settings-container">
 					<div class="user-registration-options-header">
 						<div class="user-registration-options-header--bottom" >
 							<div class="ur-scroll-ui">
 								<div class="ur-scroll-ui__scroll-nav">
 									<ul class="subsubsub  ur-scroll-ui__items">
-										<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=user-registration' ) ); ?>" class="current ur-scroll-ui__item">Registration Forms</a></li>
+										<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=user-registration' ) ); ?>" class="current ur-scroll-ui__item active">Registration Forms</a></li>
 										<li><a href="<?php echo esc_url( admin_url( 'admin.php?page=user-registration&tab=login-forms' ) ); ?>" class=" ur-scroll-ui__item">Login Forms</a></li>
 									</ul>
 								</div>
@@ -176,7 +177,6 @@ class UR_Admin_Registrations_Table_List extends UR_List_Table {
 						</div>
 					</div>
 				</div>
-				<a href="<?php echo esc_url( admin_url( 'admin.php?page=add-new-registration' ) ); ?>" class="page-title-action"><?php esc_html_e( 'Add New', 'user-registration' ); ?></a>
 				<hr class="wp-header-end">
 				<form id="registration-list" method="get">
 					<input type="hidden" name="page" value="user-registration" />
