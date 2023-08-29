9f589c5507015169b7bac91750205b8e01f3e1a9
Milan Chaudhary <chaudharymilan996@gmail.com>
2023-08-29 10:33:16 +0545

    Add - Login Wrapper

--- includes/admin/views/html-login-page-forms.php
+++ includes/admin/views/html-login-page-forms.php
@@ -34,60 +34,62 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</div>
 	<hr class="wp-header-end">
 </div>
-<div class="wrap user-registration__wrap ur-form-container">
-	<h1 style="display:none"></h1> <!-- To manage notices -->
-	<div id="menu-management-liquid" class="ur-form-subcontainer">
-		<!-- <div class="ur-loading-container">
-			<div class="ur-circle-loading"></div>
-		</div> -->
-		<div id="menu-management">
-			<div class="menu-edit">
-				<div id="post-body">
-					<div class="ur-registered-from">
-						<div class='ur-builder-wrapper'>
-							<div class="ur-selected-inputs">
-								<div class="ur-builder-wrapper-content ur-login-form-wrapper">
-									<div class="ur-login-shortcode">
-										<h1><?php echo esc_html__( 'Login Shortcode', 'user-registration' ); ?></h1>
-										<div class="ur-login-shortcode--wrapper">
-										<img src="<?php echo esc_url( UR()->plugin_url() . '/assets/images/login_form.png' ); ?>" alt="Login Form">
-										<div class="ur-login-content">
-											<?php printf( '<p>%s <strong>%s</strong> %s</p>', esc_html( 'You can add this shortcode', 'user-registration' ), esc_html__( '[user_registration_login]', 'user-registration' ), esc_html__( 'in the pages where you want to show your login form which is provided below:-', 'user-registration' ) ); ?>
-											<div class="major-publishing-actions wp-clearfix">
-												<div class="login-forms-shortcode-action shortcode">
-													<input type="text" onfocus="this.select();" readonly="readonly"
-													value='[user_registration_login]'
-													class="widefat code" size="35"></span>
+<div class="user-registration-login-form-container">
+	<div class="wrap user-registration__wrap ur-form-container">
+		<h1 style="display:none"></h1> <!-- To manage notices -->
+		<div id="menu-management-liquid" class="ur-form-subcontainer">
+			<!-- <div class="ur-loading-container">
+				<div class="ur-circle-loading"></div>
+			</div> -->
+			<div id="menu-management">
+				<div class="menu-edit">
+					<div id="post-body">
+						<div class="ur-registered-from">
+							<div class='ur-builder-wrapper'>
+								<div class="ur-selected-inputs">
+									<div class="ur-builder-wrapper-content ur-login-form-wrapper">
+										<div class="ur-login-shortcode">
+											<h1><?php echo esc_html__( 'Login Shortcode', 'user-registration' ); ?></h1>
+											<div class="ur-login-shortcode--wrapper">
+											<img src="<?php echo esc_url( UR()->plugin_url() . '/assets/images/login_form.png' ); ?>" alt="Login Form">
+											<div class="ur-login-content">
+												<?php printf( '<p>%s <strong>%s</strong> %s</p>', esc_html( 'You can add this shortcode', 'user-registration' ), esc_html__( '[user_registration_login]', 'user-registration' ), esc_html__( 'in the pages where you want to show your login form which is provided below:-', 'user-registration' ) ); ?>
+												<div class="major-publishing-actions wp-clearfix">
+													<div class="login-forms-shortcode-action shortcode">
+														<input type="text" onfocus="this.select();" readonly="readonly"
+														value='[user_registration_login]'
+														class="widefat code" size="35"></span>
 
-													<button id="copy-shortcode" class="button ur-copy-shortcode help_tip" href="#" data-tip="<?php esc_attr_e( 'Copy Shortcode ! ', 'user-registration' ); ?>" data-copied="<?php esc_attr_e( 'Copied ! ', 'user-registration' ); ?>">
-														<span class="dashicons dashicons-admin-page"></span>
-													</button>
+														<button id="copy-shortcode" class="button ur-copy-shortcode help_tip" href="#" data-tip="<?php esc_attr_e( 'Copy Shortcode ! ', 'user-registration' ); ?>" data-copied="<?php esc_attr_e( 'Copied ! ', 'user-registration' ); ?>">
+															<span class="dashicons dashicons-admin-page"></span>
+														</button>
+													</div>
 												</div>
-											</div>
-											<?php printf( '<p>%s <strong>%s</strong> %s</p>', esc_html( 'If you want to create your own my account page, you can create a new page and You can add this shortcode', 'user-registration' ), esc_html__( '[user_registration_myaccount]', 'user-registration' ), esc_html__( 'in the pages where you want to show your myaccount login form which is provided below:-', 'user-registration' ) ); ?>
-											<div class="major-publishing-actions wp-clearfix">
-												<div class="login-forms-shortcode-action shortcode">
-													<input type="text" onfocus="this.select();" readonly="readonly"
-													value='[user_registration_myaccount]'
-													class=" code" size="35"></span>
+												<?php printf( '<p>%s <strong>%s</strong> %s</p>', esc_html( 'If you want to create your own my account page, you can create a new page and You can add this shortcode', 'user-registration' ), esc_html__( '[user_registration_myaccount]', 'user-registration' ), esc_html__( 'in the pages where you want to show your myaccount login form which is provided below:-', 'user-registration' ) ); ?>
+												<div class="major-publishing-actions wp-clearfix">
+													<div class="login-forms-shortcode-action shortcode">
+														<input type="text" onfocus="this.select();" readonly="readonly"
+														value='[user_registration_myaccount]'
+														class=" code" size="35"></span>
 
-													<button id="copy-shortcode" class="button ur-copy-shortcode help_tip" href="#" data-tip="<?php esc_attr_e( 'Copy Shortcode ! ', 'user-registration' ); ?>" data-copied="<?php esc_attr_e( 'Copied ! ', 'user-registration' ); ?>">
-														<span class="dashicons dashicons-admin-page"></span>
-													</button>
+														<button id="copy-shortcode" class="button ur-copy-shortcode help_tip" href="#" data-tip="<?php esc_attr_e( 'Copy Shortcode ! ', 'user-registration' ); ?>" data-copied="<?php esc_attr_e( 'Copied ! ', 'user-registration' ); ?>">
+															<span class="dashicons dashicons-admin-page"></span>
+														</button>
+													</div>
 												</div>
 											</div>
 										</div>
-									</div>
-									</div>
-									<div class="ur-login-view-doc">
-										<a href="<?php echo esc_url_raw( 'https://docs.wpuserregistration.com/docs/how-to-show-login-form/' ); ?>" target="_blank"><?php echo esc_html__( 'View Documentation For More Details', 'user-registration' ); ?></a>
+										</div>
+										<div class="ur-login-view-doc">
+											<a href="<?php echo esc_url_raw( 'https://docs.wpuserregistration.com/docs/how-to-show-login-form/' ); ?>" target="_blank"><?php echo esc_html__( 'View Documentation For More Details', 'user-registration' ); ?></a>
+										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				</div><!-- /#post-body -->
-			</div><!-- /.menu-edit -->
-		</div><!-- /#menu-management -->
+					</div><!-- /#post-body -->
+				</div><!-- /.menu-edit -->
+			</div><!-- /#menu-management -->
+		</div>
 	</div>
 </div>
