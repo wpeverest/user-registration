8d2f79e88b9eaac14f56217ec2be14a511c3d7f5
Ranjan <khanalranjan43@gmail.com>
2023-10-13 08:20:39 +0545

    Limit length feature frontend validation

--- includes/functions-ur-template.php
+++ includes/functions-ur-template.php
@@ -204,21 +204,29 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 		if ( ! empty( $args['min'] ) || '0' === $args['min'] ) {
 			$args['custom_attributes']['min'] = $args['min'];
 		}
-				
-		if ( ! empty( $args['min-characters'] ) || '0' === $args['min-characters'] ) {
-			$args['custom_attributes']['min'] = $args['min-characters'];
+	
+		if ( isset( $args['min-characters'] ) ) {
+			if ( ! empty( $args['min-characters'] ) || '0' === $args['min-characters'] ) {
+				$args['custom_attributes']['minlength'] = $args['min-characters'];
+			}
 		}
 
-		if ( ! empty( $args['max-characters'] ) || '0' === $args['max-characters'] ) {
-			$args['custom_attributes']['maxlength'] = $args['max-characters'];
+		if ( isset( $args['max-characters'] ) ) {
+			if ( ! empty( $args['max-characters'] ) || '0' === $args['max-characters'] ) {
+				$args['custom_attributes']['maxlength'] = $args['max-characters'];
+			}
 		}
 
-		if ( ! empty( $args['min-words'] ) || '0' === $args['min-words'] ) {
-			$args['custom_attributes']['min-words'] = $args['min-words'];
+		if ( isset( $args['min-words'] ) ) {
+			if ( ! empty( $args['min-words'] ) || '0' === $args['min-words'] ) {
+				$args['custom_attributes']['min-words'] = $args['min-words'];
+			}
 		}
 
-		if ( ! empty( $args['max-words'] ) || '0' === $args['max-words'] ) {
-			$args['custom_attributes']['max-words'] = $args['max-words'];
+		if ( isset( $args['max-words'] ) ) {
+			if ( ! empty( $args['max-words'] ) || '0' === $args['max-words'] ) {
+				$args['custom_attributes']['max-words'] = $args['max-words'];
+			}
 		}
 
 		if ( ! empty( $args['max'] ) || '0' === $args['max'] ) {
