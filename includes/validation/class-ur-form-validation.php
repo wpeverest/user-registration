0bccb3022b74b9943a17552ddf33e25c39c88370
dipuchaudhary <dipuchaudhary8@gmail.com>
2024-01-25 14:00:56 +0545

    updated

--- includes/validation/class-ur-form-validation.php
+++ includes/validation/class-ur-form-validation.php
@@ -605,8 +605,8 @@ class UR_Form_Validation extends UR_Validation {
 		);
 
 		$invisible_field_names = array_column( $filteredfields, 'general_setting' );
-		$invisibleFieldNames   = array_column( $invisible_field_names, 'field_name' ); //phpcs:ignore;
-		$required_fields       = array_diff( $required_fields, $invisibleFieldNames );
+		$invisible_field_names = array_column( $invisible_field_names, 'field_name' ); //phpcs:ignore;
+		$required_fields       = array_diff( $required_fields, $invisible_field_names );
 
 		if ( array_diff( $required_fields, $request_form_keys ) ) {
 			ur_add_notice( 'Some fields are missing in the submitted form. Please reload the page.', 'error' );
