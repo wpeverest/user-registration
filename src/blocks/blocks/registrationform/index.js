2ce43b9311398c4bca1c1c5241d2e0f78988f8d9
Milan Chaudhary <chaudharymilan996@gmail.com>
2024-03-06 16:27:25 +0545

    Add - User registration icon

--- src/blocks/blocks/registrationform/index.js
+++ src/blocks/blocks/registrationform/index.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { Component } from "react";
 import metadata from "./block.json";
+import { Icon } from "./components/Icon";
 
 export const name = metadata.name;
 export const settings = {
 	...metadata,
-	icon: "smiley",
+	icon: Icon,
 	edit: function (props) {
 		return "User registraion form block";
 	},
