187d9b158972735d710918344867d64f2ce8f17a
Milan Chaudhary <chaudharymilan996@gmail.com>
2024-03-18 14:36:02 +0545

    Fix - Pro blocks including methods

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -2,5 +2,20 @@ import registerBlocks from "./blocks";
 import "./editor.scss";
 import "./style.scss";
 
+const { isPro } = typeof _UR_DASHBOARD_ !== "undefined" && _UR_DASHBOARD_;
+
 //Register the blocks.
 registerBlocks();
+if (isPro) {
+	let registerProBlocks;
+
+	try {
+		registerProBlocks = require("./blocks/pro/blocks").default;
+	} catch (error) {
+		console.error("Failed to import registerProBlocks:", error);
+	}
+
+	if (registerProBlocks) {
+		registerProBlocks();
+	}
+}
