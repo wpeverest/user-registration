d142723ce5d5b4b6aca349ae2997ba96924310d9
Prajjwal Poudel <85762481+iamprazol@users.noreply.github.com>
2024-04-08 14:57:04 +0545

    Fix - Build issues

--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,8 @@ module.exports = (env, argv) => {
 
 	return {
 		entry: {
-			welcome: "/src/welcome/index.js",
-			dashboard: "/src/dashboard/index.js",
+			welcome: "./src/welcome/index.js",
+			dashboard: "./src/dashboard/index.js",
 			formblock: "./assets/js/admin/gutenberg/form-block.js",
 			blocks: "./src/blocks/index.js",
 		},
