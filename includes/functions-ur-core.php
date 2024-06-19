55879620d98424fb111829c66e7df7a500afe177
Samyog subedi <officialsamyogsubedi@gmail.com>
2024-06-19 10:20:38 +0545

    Feature - Akismet Integration.

--- includes/functions-ur-core.php
+++ includes/functions-ur-core.php
@@ -1315,6 +1315,22 @@ function ur_admin_form_settings_fields( $form_id ) {
 				'default'           => ur_get_single_post_meta( $form_id, 'user_registration_form_setting_redirect_after', '2' ),
 				'tip'               => __( 'Time to wait after registration before redirecting user to another page.', 'user-registration' ),
 			),
+			array(
+				'type'              => 'toggle',
+				'label'             => __( 'Activate Spam Protection By Akismet', 'user-registration' ),
+				'required'          => false,
+				'id'                => 'user_registration_enable_akismet',
+				'class'             => array( 'ur-enhanced-select' ),
+				'custom_attributes' => array(),
+				'default'           => ur_get_single_post_meta( $form_id, 'user_registration_enable_akismet', false ),
+				'tip'               => __( 'Enable anti-spam for this form with akismet.', 'user-registration' ),
+
+			),
+			array(
+				'type'        => 'label',
+				'id'          => 'user_registration_akismet_warning',
+				'description' => ur_check_akismet_installation(),
+			),
 		),
 	);
 	/**
@@ -4813,5 +4829,169 @@ if ( ! function_exists( 'ur_redirect_to_addons_page' ) ) {
 			wp_safe_redirect( esc_url_raw( admin_url( 'admin.php?page=user-registration-dashboard#features' ) ) );
 			exit;
 		}
+	}
+}
+if ( ! function_exists( 'ur_check_akismet_installation' ) ) {
+
+	/**
+	 * Check the configuration status of Akismet.
+	 *
+	 * @return string The status message indicating whether Akismet is installed, activated, or configured.
+	 */
+	function ur_check_akismet_installation() {
+		$warning_color = '#FF8C00';
+
+		if ( ! file_exists( WP_PLUGIN_DIR . '/akismet/akismet.php' ) ) {
+			return sprintf(
+				'<div><span style="color: %s;">%s</span>%s%s%s</div>',$warning_color,esc_html__( 'Warning:- ', 'user-registration' ),esc_html__( ' This feature is inactive because Akismet plugin has', 'user-registration' ),'<a href="' . esc_url( admin_url( 'plugins.php' ) ) . '" target="_blank">' . esc_html__( ' not been installed.', 'user-registration' ) . '</a>',esc_html__( ' For more', 'user-registration' ),	'<a href="https://docs.wpuserregistration.com/" target="_blank">' . esc_html__( ' information', 'user-registration' ) . '</a>'
+			);
+		} elseif ( ! is_plugin_active( 'akismet/akismet.php' ) ) {
+			return sprintf(
+				'<div><span style="color: %s;">%s</span>%s%s%s%s</div>',$warning_color,	esc_html__( 'Warning:- ', 'user-registration' ),esc_html__( ' This feature is inactive because Akismet plugin has', 'user-registration' ),'<a href="' . esc_url( admin_url( 'plugins.php' ) ) . '" target="_blank">' . esc_html__( ' not been activated', 'user-registration' ) . '</a>',	esc_html__( ' For more ', 'user-registration' ),'<a href="https://docs.wpuserregistration.com/" target="_blank">' . esc_html__( ' information', 'user-registration' ) . '</a>'
+			);
+		} elseif ( ! ur_is_akismet_configured() ) {
+			return sprintf(
+				'<div><span style="color: %s;">%s</span>%s%s%s</div>',$warning_color,esc_html__( 'Warning:- ', 'user-registration' ),esc_html__( ' This feature is inactive because Akismet plugin has not been properly configured.', 'user-registration' ),esc_html__( ' For more', 'user-registration' ),'<a href="https://docs.wpuserregistration.com/" target="_blank">' . esc_html__( ' information', 'user-registration' ) . '</a>'
+			);
+		}
+	}
+
+}
+
+if ( ! function_exists( 'ur_is_akismet_configured' ) ) {
+
+	/**
+	 * Has the Akismet plugin been configured wih a valid API key?
+	 *
+	 * @since 4.2.1.2
+	 *
+	 * @return bool
+	 */
+	function ur_is_akismet_configured() {
+
+		if ( ! is_plugin_active( 'akismet/akismet.php' ) ) {
+			return false;
+		}
+		require_once WP_PLUGIN_DIR . '/akismet/akismet.php';
+
+		$akismet_instance = new Akismet();
+		// Akismet will only allow an API key to be saved if it is a valid key.
+		// We can assume that if there is an API key saved, it is valid.
+		$akismet_api_key = $akismet_instance->get_api_key();
+
+		if ( ! empty( $akismet_api_key ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+
+}
+add_filter( 'user_registration_get_akismet_validate', 'ur_get_akismet_validate', 10, 2 );
+if ( ! function_exists( 'ur_get_akismet_validate' ) ) {
+	/**
+	 * Check if registration should be validated by Akismet for potential spam.
+	 *
+	 * This function checks whether the Akismet plugin is installed and configured, and if the Akismet
+	 * validation option is enabled for a specific form. If validation is enabled, it prepares the
+	 * necessary data for the validation request and sends it to Akismet's 'registration-check' endpoint.
+	 *
+	 * @param int   $form_id The form_id to check if to validate.
+	 * @param array $form_data  values to validate.
+	 *
+	 * @return bool
+	 *   - true if the form_data is potentially spam according to Akismet.
+	 *   - false if Akismet validation is not enabled, the plugin is not properly configured, or the form_data is not considered spam.
+	 */
+	function ur_get_akismet_validate( $form_id, $form_data ) {
+		if ( ! file_exists( WP_PLUGIN_DIR . '/akismet/akismet.php' ) ) {
+			return false;
+		}
+
+		if ( ! ur_is_akismet_configured() ) {
+			return false;
+		}
+
+		$is_akismet_enabled = ur_get_single_post_meta( $form_id, 'user_registration_enable_akismet' );
+		if ( $is_akismet_enabled ) {
+
+			$form_content = ur_get_form_data_for_akismet( $form_data );
+
+			$request = array(
+				'blog'                 => home_url(),
+				'user_ip'              => ur_get_ip_address(),
+				'user_agent'           => isset( $_SERVER['HTTP_USER_AGENT'] ) ? wp_unslash( $_SERVER['HTTP_USER_AGENT'] ) : null, // phpcs:ignore
+				'referrer'             => wp_get_referer() ? wp_get_referer() : null,
+				'permalink'            => evf_current_url(),
+				'comment_type'         => 'registration',
+				'comment_author'       => isset( $form_content['user_login'] ) ? $form_content['user_login'] : '',
+				'comment_author_email' => isset( $form_content['user_email'] ) ? $form_content['user_email'] : '',
+				'comment_author_url'   => isset( $form_content['user_url'] ) ? $form_content['user_url'] : '',
+				'comment_content'      => isset( $form_content['other_content'] ) ? $form_content['other_content'] : '',
+				'blog_lang'            => get_locale(),
+				'blog_charset'         => get_bloginfo( 'charset' ),
+			);
+
+			$response = Akismet::http_post( build_query( $request ), 'comment-check' );
+			return ! empty( $response ) && isset( $response[1] ) && 'true' === trim( $response[1] );
+		}
+
+		return false;
+	}
+}
+
+if ( ! function_exists( 'ur_get_form_data_for_akismet' ) ) {
+	function ur_get_form_data_for_akismet( $form_data ) {
+		$field_type_allowlist = ur_get_allowed_field_for_akisment();
+		$entry_data           = array();
+		$form_content         = array();
+		$other_content        = array();
+
+		foreach ( $form_data as $key => $field ) {
+			if ( isset( $field->extra_params['field_key'] ) ) {
+				$entry_data[ $field->extra_params['field_key'] ] = $field->value;
+			}
+		}
+
+		foreach ( $entry_data as $field_key => $value ) {
+			if ( in_array( $field_key, $field_type_allowlist ) ) {
+				switch ( $field_key ) {
+					case 'user_email':
+						$form_content['user_email'] = $value;
+						break;
+					case 'user_url':
+						$form_content['user_url'] = $value;
+						break;
+					case 'user_login':
+						$form_content['user_login'] = $value;
+						break;
+					default:
+						$other_content[] = $value;
+						break;
+				}
+			}
+		}
+
+		if ( ! empty( $other_content ) ) {
+			$form_content['other_content'] = implode( ' ', $other_content );
+		}
+		return $form_content;
+	}
+}
+if ( ! function_exists( 'ur_get_allowed_field_for_akisment' ) ) {
+	function ur_get_allowed_field_for_akisment() {
+		$field_type_allowlist = array(
+			'user_login',
+			'text',
+			'textarea',
+			'user_email',
+			'phone',
+			'address',
+			'user_url',
+			'wysiwyg',
+			'description',
+		);
+		return $field_type_allowlist;
 	}
 }
