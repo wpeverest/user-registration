90c1a2ad03707504beb5553087dcbcf8888f53d6
y000yal <yoyal.limbu@gmail.com>
2024-11-15 10:14:24 +0545

    Added field id to target which field to apply custom filter

--- includes/functions-ur-template.php
+++ includes/functions-ur-template.php
@@ -780,7 +780,7 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 				$backtrace = debug_backtrace();
 				$parent_function = isset($backtrace[1]) ? $backtrace[1]['function'] : '';
 
-				$args['options'] =  ($parent_function === 'frontend_includes') ? apply_filters('override_options_for_select_field', $args['options']) : $args['options'];
+				$args['options'] =  ($parent_function === 'frontend_includes') ? apply_filters('override_options_for_select_field', $args['options'] , $args['id']) : $args['options'];
 
 				if ( ! empty( $args['options'] ) ) {
 					// If we have a blank option, select2 needs a placeholder.
