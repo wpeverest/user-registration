acf5ddb65825ccb9ba2c019725756c954d21b60d
y000yal <yoyal.limbu@gmail.com>
2024-11-15 10:08:23 +0545

    checked if the function is called from frontend to use the filter

--- includes/functions-ur-template.php
+++ includes/functions-ur-template.php
@@ -777,6 +777,7 @@ if ( ! function_exists( 'user_registration_form_field' ) ) {
 
 				$value   = ! empty( $value ) ? $value : $default_value;
 				$options = $field .= '';
+				$backtrace = debug_backtrace();
 				$parent_function = isset($backtrace[1]) ? $backtrace[1]['function'] : '';
 
 				$args['options'] =  ($parent_function === 'frontend_includes') ? apply_filters('override_options_for_select_field', $args['options']) : $args['options'];
