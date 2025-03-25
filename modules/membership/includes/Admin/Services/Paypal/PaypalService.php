9444afd5e3da3f8ca226f9339bd1194a78fa68fd
y000yal <yoyal.limbu@gmail.com>
2025-03-25 08:17:31 +0545

    Updated paypal return to merchant subscription status for subscription type as well.

--- modules/membership/includes/Admin/Services/Paypal/PaypalService.php
+++ modules/membership/includes/Admin/Services/Paypal/PaypalService.php
@@ -145,7 +145,7 @@ class PaypalService {
 
 		$is_order_updated = $this->members_orders_repository->update( $member_order['ID'], array( 'status' => 'completed' ) );
 
-		if ( $is_order_updated && 'paid' === $member_order['order_type'] ) {
+		if ( $is_order_updated && ('paid' === $member_order['order_type'] || 'subscription' === $member_order['order_type'] ) ) {
 			$member_subscription = $this->members_subscription_repository->get_member_subscription( $member_id );
 			$this->members_subscription_repository->update( $member_subscription['ID'], array( 'status' => 'active' ) );
 			$logger->notice( 'Return to merchant log' . $member_subscription['ID'], array( 'source' => 'ur-membership-paypal' ) );
