b7e548e4e946ce117d9e5bc3bfbebcc55b72b645
y000yal <yoyal.limbu@gmail.com>
2025-05-13 10:58:30 +0545

    Added Membership block icon.

--- src/blocks/blocks/membership-listing/index.js
+++ src/blocks/blocks/membership-listing/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import metadata from "./block.json";
-import { EditPassword } from "./../../components/Icon";
+import { MembershipListing } from "./../../components/Icon";
 import Edit from "./Edit";
 import Save from "./Save";
 
 export const name = metadata.name;
 export const settings = {
 	...metadata,
-	icon: EditPassword,
+	icon: MembershipListing,
 	edit: Edit,
 	save: Save,
 };
