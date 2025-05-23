fcf77722fb2369bfdade7df14f4d3d5f25440cc3
y000yal <yoyal.limbu@gmail.com>
2025-05-23 16:24:36 +0545

    Updated membership thank you page notice logic.

--- modules/membership/includes/Templates/thank-you-page.php
+++ modules/membership/includes/Templates/thank-you-page.php
@@ -11,8 +11,9 @@ $footer           = ! empty( $attributes['footer'] ) ? $attributes['footer'] : "
 $notice_message   = ! empty( $attributes['notice_message'] ) ? $attributes['notice_message'] : "For paid memberships there might be a delay of few minutes for your subscription status to be updated by the payment gateways.";
 $transaction_info = ! empty( $attributes['transaction_info'] ) ? $attributes['transaction_info'] : "Please use this transaction/order id for support regarding payments if needed.";
 $is_preview       = ! empty( $attributes['is_preview'] ) ? $attributes['is_preview'] : false;
-$show_notice_1    = ! empty( $attributes['show_notice_1'] ) ? $attributes['show_notice_1'] : false;
-$show_notice_2    = ! empty( $attributes['show_notice_2'] ) ? $attributes['show_notice_2'] : false;
+$show_notice_1    = isset( $attributes['show_notice_1'] ) ? $attributes['show_notice_1'] : true;
+$show_notice_2    = isset( $attributes['show_notice_2'] ) ? $attributes['show_notice_2'] : true;
+
 ?>
 <!--order successful section-->
 <div id="order-complete-section" class="thank-you-page-container">
@@ -20,7 +21,7 @@ $show_notice_2    = ! empty( $attributes['show_notice_2'] ) ? $attributes['show_
 		<p><?php echo __( $header, 'user-registration' ); ?></p>
 		<?php
 		$is_payment_done = ( isset( $_GET['payment_type'] ) && ! empty( $_GET['payment_type'] ) && 'paid' === $_GET['payment_type'] );
-		if ( $is_preview || $is_payment_done ) :
+		if ( $is_preview || $is_payment_done || $show_notice_1) :
 			?>
 			<?php
 			if ( $show_notice_1 || $is_payment_done ):
@@ -48,28 +49,11 @@ $show_notice_2    = ! empty( $attributes['show_notice_2'] ) ? $attributes['show_
 				<p>
 					<?php echo __( $transaction_info, 'user-registration' ); ?>
 				</p>
-				<?php
-				if ( ! $is_preview ):
-					?>
+
 					<p>
 						<?php echo __( "Transaction ID : ", "user-registration" ) ?>
 						<b><i><?php echo $transaction_id; ?></i></b>
 					</p>
-				<?php
-				endif;
-				?>
-			</div>
-		<?php
-		else :
-			?>
-			<div class="thank-you-notice info">
-				<p>
-					<?php echo __( $transaction_info, 'user-registration' ); ?>
-				</p>
-				<p>
-					<?php echo __( "Transaction ID : ", "user-registration" ) ?>
-					<b><i><?php echo $transaction_id; ?></i></b>
-				</p>
 
 			</div>
 		<?php
