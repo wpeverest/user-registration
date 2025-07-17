2c675c3e5d8437817b102def0473da49afd4dff2
Purshottam Nepal <prunepal3339@gmail.com>
2025-07-17 09:56:15 +0545

    Remove unnecessary code.

--- modules/membership/includes/Admin/Services/MembershipService.php
+++ modules/membership/includes/Admin/Services/MembershipService.php
@@ -308,21 +308,12 @@ class MembershipService {
 		 * This hook should be used by new payment gateway integrations add-on to validate the membership data.
 		 *
 		 * @param array $result Membership validation result data
+		 * @param array $data Membership data.
 		 *
 		 * @since 4.2.3
 		 *
 		 */
-		return apply_filters( 'user_registration_membership_validate_membership_data', $result );
-		/**
-		 * Filters the membership data validation result
-		 *
-		 * This hook should be used by new payment gateway integrations add-on to validate the membership data.
-		 *
-		 * @since 4.2.3
-		 *
-		 * @param array $result Membership validation result data
-		 */
-		return apply_filters( 'user_registration_membership_validate_membership_data', $result );
+		return apply_filters( 'user_registration_membership_validate_membership_data', $result, $data );
 	}
 
 	/**
