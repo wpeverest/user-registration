dca65b031865fd9dce7d2388e15ce5cec48820c6
y000yal <yoyal.limbu@gmail.com>
2025-07-24 09:10:04 +0545

    Added session to show error after redirection in login form.

--- templates/myaccount/form-login.php
+++ templates/myaccount/form-login.php
@@ -83,7 +83,18 @@ do_action( 'user_registration_before_customer_login_form' );
  * @param function Print notice function.
  * @return function.
  */
-ur_add_notice( apply_filters( 'user_registration_post_login_errors', '' ), 'error' );
+
+// Check for session error message (PRG pattern)
+if ( ! session_id() ) {
+	session_start();
+}
+if ( isset( $_SESSION['ur_login_error'] ) ) {
+	ur_add_notice( $_SESSION['ur_login_error'], 'error' );
+	unset( $_SESSION['ur_login_error'] );
+} else {
+	ur_add_notice( apply_filters( 'user_registration_post_login_errors', '' ), 'error' );
+}
+
 if ( ! $is_passwordless_enabled || $is_passwordless_login_default_login_area_enabled ) {
 	ur_add_notice( apply_filters( 'user_registration_passwordless_login_notice', '' ), 'success' );
 }
@@ -144,7 +155,7 @@ apply_filters( 'user_registration_login_form_before_notice', ur_print_notices() 
 						}
 						?>
 						<span class="input-wrapper">
-						<input placeholder="<?php echo esc_attr( $placeholders['username'] ); ?>" type="text" class="user-registration-Input user-registration-Input--text input-text" name="username" id="username" value="<?php echo ( ! empty( $_POST['username'] ) ) ? esc_attr( wp_unslash( sanitize_text_field( $_POST['username'] ) ) ) : ''; // phpcs:ignore ?>" style="<?php echo ($enable_field_icon || $is_login_settings && is_plugin_active( 'user-registration-pro/user-registration.php' )) ? "padding-left: 32px !important" : '' ?>"/>
+						<input placeholder="<?php echo esc_attr( $placeholders['username'] ); ?>" type="text" class="user-registration-Input user-registration-Input--text input-text" name="username" id="username" value="" style="<?php echo ($enable_field_icon || $is_login_settings && is_plugin_active( 'user-registration-pro/user-registration.php' )) ? "padding-left: 32px !important" : '' ?>"/>
 						<?php if ( $enable_field_icon || $is_login_settings && is_plugin_active( 'user-registration-pro/user-registration.php' ) ) { ?>
 							<span class="ur-icon ur-icon-user">
 
