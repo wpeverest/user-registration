adb6260ceb33fa57bfa6697ea41f2b54c9e94739
Ayush Shrestha <ayushstha143@gmail.com>
2025-08-19 17:37:23 +0545

    navigation menu fixed in responsive

--- includes/admin/functions-ur-admin.php
+++ includes/admin/functions-ur-admin.php
@@ -1100,8 +1100,13 @@ if ( ! function_exists( 'user_registration_plugin_responsive_main_header' ) ) {
 			</div>
 			<div class="user-registration-hamburger-menu--body">
 				<div class="ur-hamburger-menu-close">
-					<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+					<!-- <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
 						<path fill-rule="evenodd" clip-rule="evenodd" d="M10.9393 12L6.9696 15.9697L8.03026 17.0304L12 13.0607L15.9697 17.0304L17.0304 15.9697L13.0607 12L17.0303 8.03039L15.9696 6.96973L12 10.9393L8.03038 6.96973L6.96972 8.03039L10.9393 12Z" fill="#080341"/>
+					</svg> -->
+
+					<svg xmlns="http://www.w3.org/2000/svg" fill="#000" viewBox="0 0 24 24">
+						<path d="M19.561 2.418a1.428 1.428 0 1 1 2.02 2.02L4.44 21.583a1.428 1.428 0 1 1-2.02-2.02L19.56 2.418Z"/>
+						<path d="M2.418 2.418a1.428 1.428 0 0 1 2.02 0l17.144 17.143a1.428 1.428 0 1 1-2.02 2.02L2.418 4.44a1.428 1.428 0 0 1 0-2.02Z"/>
 					</svg>
 				</div>
 				<ul class="user-registration-hamburger-menu--body__items">
