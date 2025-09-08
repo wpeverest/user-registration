1a40944cc0b061afec2644ec9bff5152fb0a8874
y000yal <yoyal.limbu@gmail.com>
2025-09-08 17:09:59 +0545

    FInished till subscription and order creation.

--- includes/Functions/CoreFunctions.php
+++ includes/Functions/CoreFunctions.php
@@ -370,16 +370,20 @@ if ( ! function_exists( 'build_membership_list_frontend' ) ) {
 		$symbol                  = $currencies[ $currency ]['symbol'];
 		$new_mem                 = array();
 		$active_payment_gateways = array();
+
 		foreach ( $memberships as $k => $membership ) {
+			$membership_id = ! empty( $membership['ID'] ) ? $membership['ID'] : '';
+			$membership_meta_value = !empty($membership['meta_value']) ? $membership['meta_value'] : '';
+			$membership_type = (!empty($membership_meta_value) && !empty($membership_meta_value['type'])) ? $membership_meta_value['type'] : '';
 			$new_mem[ $k ] = array(
-				'ID'                => $membership['ID'],
-				'title'             => $membership['post_title'],
-				'description'       => ! empty( $membership['post_content']['description'] ) ? $membership['post_content']['description'] : get_post_meta( $membership['ID'], 'ur_membership_description', true ),
-				'type'              => $membership['meta_value']['type'],
-				'amount'            => $membership['meta_value']['amount'] ?? 0,
+				'ID'                => $membership_id,
+				'title'             => ! empty( $membership['post_title'] ) ? $membership['post_title'] : '',
+				'description'       => ! empty( $membership['post_content']['description'] ) ? $membership['post_content']['description'] : get_post_meta( $membership_id, 'ur_membership_description', true ),
+				'type'              => $membership_type,
+				'amount'            => !empty($membership_meta_value) ? $membership['meta_value']['amount'] : 0,
 				'currency_symbol'   => $symbol,
-				'calculated_amount' => 'free' === $membership['meta_value']['type'] ? 0 : round( $membership['meta_value']['amount'] ),
-				'period'            => 'free' === $membership['meta_value']['type'] ? __( 'Free', 'user-registration' ) : ( 'subscription' === $membership['meta_value']['type'] ? $symbol . $membership['meta_value']['amount'] . ' / ' . number_format( $membership['meta_value']['subscription']['value'] ) . ' ' . ucfirst( $membership['meta_value']['subscription']['duration'] ) . ( $membership['meta_value']['subscription']['value'] > 1 ? '(s)' : '' ) : $symbol . round( $membership['meta_value']['amount'] ) ),
+				'calculated_amount' => 'free' === $membership_type ? 0 : (!empty($membership_meta_value) ? round( $membership_meta_value['amount'] ) : 0),
+				'period'            => 'free' === $membership_type ? __( 'Free', 'user-registration' ) : ( (!empty($membership_meta_value) && 'subscription' === $membership_meta_value['type']) ? $symbol . $membership['meta_value']['amount'] . ' / ' . number_format( $membership['meta_value']['subscription']['value'] ) . ' ' . ucfirst( $membership['meta_value']['subscription']['duration'] ) . ( $membership['meta_value']['subscription']['value'] > 1 ? '(s)' : '' ) : $symbol . (!empty($membership_meta_value) ? round( $membership['meta_value']['amount'] ) : 0) ),
 			);
 			if ( isset( $membership['meta_value']['payment_gateways'] ) ) {
 				foreach ( $membership['meta_value']['payment_gateways'] as $key => $gateways ) {
