3002085f2cccba96babef74ff2f7303cc4053a9f
Purshottam Nepal <prunepal3339@gmail.com>
2025-09-21 14:07:03 +0545

    Backward compatibility while fetching field name.

--- includes/abstracts/abstract-ur-form-field.php
+++ includes/abstracts/abstract-ur-form-field.php
@@ -252,17 +252,17 @@ abstract class UR_Form_Field {
 			}
 			$form_data['custom_attributes']['data-locale'] = $date_localization;
 		}
-
-		$form_data['custom_attributes']['data-label'] = ur_string_translation( $form_id, 'user_registration_' . $data['advance_setting']->field_name . '_label', $data['general_setting']->label );
+		$field_name = isset( $data[ 'advance_setting' ]->field_name ) ? $data[ 'advance_setting' ]->field_name : $data[ 'general_setting' ]->field_name;
+		$form_data['custom_attributes']['data-label'] = ur_string_translation( $form_id, 'user_registration_' . $field_name . '_label', $data['general_setting']->label );
 
 		if ( isset( $form_data['label'] ) ) {
-			$form_data['label'] = ur_string_translation( $form_id, 'user_registration_' . $data['advance_setting']->field_name . '_label', $form_data['label'] );
+			$form_data['label'] = ur_string_translation( $form_id, 'user_registration_' . $field_name . '_label', $form_data['label'] );
 		}
 		if ( isset( $form_data['placeholder'] ) ) {
-			$form_data['placeholder'] = ur_string_translation( $form_id, 'user_registration_' . $data['advance_setting']->field_name . '_placeholder', $form_data['placeholder'] );
+			$form_data['placeholder'] = ur_string_translation( $form_id, 'user_registration_' . $field_name . '_placeholder', $form_data['placeholder'] );
 		}
 		if ( isset( $form_data['description'] ) ) {
-			$form_data['description'] = ur_string_translation( $form_id, 'user_registration_' . $data['advance_setting']->field_name . '_description', $form_data['description'] );
+			$form_data['description'] = ur_string_translation( $form_id, 'user_registration_' . $field_name . '_description', $form_data['description'] );
 		}
 
 		// Filter only selected countries for `Country` fields.
