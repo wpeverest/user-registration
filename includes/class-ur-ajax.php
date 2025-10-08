bf01bbfb767dbb779effa6566f1e9b88db591428
DAnn2012 <dannunziata@hotmail.it>
2025-10-08 12:10:25 +0200

    Update class-ur-ajax.php

--- includes/class-ur-ajax.php
+++ includes/class-ur-ajax.php
@@ -1965,7 +1965,7 @@ class UR_AJAX {
 		if ( isset( $validate_before_save['status'] ) && ! $validate_before_save['status'] ) {
 			wp_send_json_error(
 				array(
-					'message' => __( $validate_before_save['message'], "user_registration" )
+					'message' => __( $validate_before_save['message'], "user-registration" )
 				)
 			);
 		}
