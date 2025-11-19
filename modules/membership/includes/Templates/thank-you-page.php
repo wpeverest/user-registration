e26d0f70e2737ecce5fcc253790f823f1e523084
Purshottam Nepal <prunepal3339@gmail.com>
2025-11-19 13:54:06 +0545

    Allow anchor tags in thank you page header and footer.

--- modules/membership/includes/Templates/thank-you-page.php
+++ modules/membership/includes/Templates/thank-you-page.php
@@ -3,11 +3,11 @@
 $bank_data        = ( isset( $_GET['info'] ) && ! empty( $_GET['info'] ) ) ? wp_kses_post_deep( $_GET['info'] ) : '';
 $transaction_id   = ( isset( $_GET['transaction_id'] ) && ! empty( $_GET['transaction_id'] ) ) ? wp_kses_post( $_GET['transaction_id'] ) : '';
 $username         = ( isset( $_GET['username'] ) && ! empty( $_GET['username'] ) ) ? wp_kses_post( $_GET['username'] ) : '';
-$header            = ! empty( $attributes['header'] ) ? esc_html( $attributes['header'] ) : sprintf(
+$header            = ! empty( $attributes['header'] ) ? wp_kses_post( $attributes['header'] ) : sprintf(
 	__( 'Thank You! Your registration was completed successfully.', 'user-registration' ),
 	esc_html( $username )
 );
-$footer           = ! empty( $attributes['footer'] ) ? esc_html( $attributes['footer'] ) : "";
+$footer           = ! empty( $attributes['footer'] ) ? wp_kses_post( $attributes['footer'] ) : "";
 $notice_message   = ! empty( $attributes['notice_message'] ) ? esc_html( $attributes['notice_message'] ) : __("For paid memberships there might be a delay of few minutes for your subscription status to be updated by the payment gateways.", 'user-registration' );
 $transaction_info = ! empty( $attributes['transaction_info'] ) ? esc_html( $attributes['transaction_info'] ) : __("Please use this transaction/order id for support regarding payments if needed.", 'user-registration' );
 $is_preview       = ! empty( $attributes['is_preview'] ) ? $attributes['is_preview'] : false;
