8b901298b4b678e080bc22c055032e9a359ceee8
som3669 <somshrestha3669@gmail.com>
2025-12-18 14:49:27 +0545

    inital update for reset mail

--- includes/admin/settings/emails/class-ur-settings-reset-password-email.php
+++ includes/admin/settings/emails/class-ur-settings-reset-password-email.php
@@ -137,16 +137,53 @@ if ( ! class_exists( 'UR_Settings_Reset_Password_Email', false ) ) :
 			 */
 			$message = apply_filters(
 				'user_registration_reset_password_email_message',
-				sprintf(
-					__(
-						'Hi {{username}},<br/><br/>
-						We received a request to reset the password for your account on {{blog_info}}.<br/><br/>
-						If this was a mistake, simply ignore this email, and no changes will be made to your account. <br/><br/>
-						To reset your password, please click the link below: <br/>
-						<a href="{{home_url}}/{{ur_reset_pass_slug}}?action=rp&key={{key}}&login={{username}}" rel="noreferrer noopener" target="_blank">Click Here: </a><br/><br/>
-						Thank You!',
-						'user-registration'
-					)
+				__(
+					'<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; background-color: #ffffff; border-radius: 12px; overflow: hidden;">
+						<!-- Header -->
+						<div style="background-color: #E8D5FF; padding: 40px 30px; position: relative; overflow: hidden;">
+							<!-- Decorative Circles -->
+							<div style="position: absolute; top: -20px; right: 20px; width: 80px; height: 80px; background-color: rgba(232, 213, 255, 0.6); border-radius: 50%;"></div>
+							<div style="position: absolute; top: 10px; right: 100px; width: 50px; height: 50px; background-color: rgba(232, 213, 255, 0.5); border-radius: 50%;"></div>
+							<div style="position: absolute; top: 30px; right: 60px; width: 30px; height: 30px; background-color: rgba(232, 213, 255, 0.4); border-radius: 50%;"></div>
+							<!-- Logo -->
+							<div style="position: relative; z-index: 1;">
+								<svg width="48" height="48" viewBox="0 0 48 48" style="display: inline-block;">
+									<path d="M12 4C10.9 4 10 4.9 10 6v36c0 1.1.9 2 2 2h12V4H12z" fill="#4A90E2"/>
+									<path d="M24 4v40h12c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2H24z" fill="#E94B7E"/>
+								</svg>
+							</div>
+						</div>
+						
+						<!-- Body Content -->
+						<div style="padding: 40px 30px; background-color: #ffffff;">
+							<p style="margin: 0 0 20px 0; color: #000000; font-size: 16px; line-height: 1.6;">
+								Hi {{username}},
+							</p>
+							<p style="margin: 0 0 20px 0; color: #000000; font-size: 16px; line-height: 1.6;">
+								We received a request to reset the password for your account on {{blog_info}}.
+							</p>
+							<p style="margin: 0 0 20px 0; color: #000000; font-size: 16px; line-height: 1.6;">
+								If this was a mistake, simply ignore this email, and no changes will be made to your account.
+							</p>
+							<p style="margin: 0 0 20px 0; color: #000000; font-size: 16px; line-height: 1.6;">
+								To reset your password, please click the link below:
+							</p>
+							<p style="margin: 0 0 30px 0;">
+								<a href="{{home_url}}/{{ur_reset_pass_slug}}?action=rp&key={{key}}&login={{username}}" rel="noreferrer noopener" target="_blank" style="color: #4A90E2; text-decoration: none; font-weight: 600;">Click Here</a>
+							</p>
+						</div>
+						
+						<!-- Footer -->
+						<div style="padding: 30px; background-color: #ffffff; border-top: 1px solid #e0e0e0;">
+							<p style="margin: 0 0 10px 0; color: #000000; font-size: 16px; line-height: 1.6;">
+								Thank You!
+							</p>
+							<p style="margin: 0; color: #000000; font-size: 16px; line-height: 1.6; font-weight: 600;">
+								{{blog_info}} Team
+							</p>
+						</div>
+					</div>',
+					'user-registration'
 				)
 			);
 
