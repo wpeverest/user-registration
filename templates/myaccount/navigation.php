f2ab84d8a6a91583b5ac0c326dc6439650fdf15a
Prajjwal Poudel <iamprazol@gmail.com>
2025-12-25 12:04:01 +0545

    Fix - Margin and message issue

--- templates/myaccount/navigation.php
+++ templates/myaccount/navigation.php
@@ -30,7 +30,7 @@ $layout = get_option( 'user_registration_my_account_layout', 'vertical' );
 if ( 'vertical' === $layout ) {
 	?>
 	<div class="user-registration-MyAccount-navigation--wrapper">
-		<div class='user-registration-profile-header'>
+		<div class='user-registration-profile-header-nav'>
 			<div class='user-registration-img-container'>
 				<?php
 				$gravatar_image      = get_avatar_url( get_current_user_id(), $args = null );
