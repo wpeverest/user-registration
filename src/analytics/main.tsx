19ffe1e5bdb8e9e75723081ba906dcc4e2cecd17
asheshmagar <ashesh.ido@gmail.com>
2025-12-31 10:19:26 +0545

    feat: make ready for PRO

--- src/analytics/main.tsx
+++ src/analytics/main.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './main.scss';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ProgressIndicator } from './components/progress-indicator';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./main.scss";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ProgressIndicator } from "./components/progress-indicator";
 
-const rootElement = document.getElementById('UR-Pro-Analytics-Root');
+const rootElement = document.getElementById("UR-Pro-Analytics-Root");
 const queryClient = new QueryClient();
+
+const ProApp = React.lazy(async () => {
+	try {
+		// @ts-ignore
+		return await import("./ProApp");
+	} catch {
+		return {
+			default: App
+		};
+	}
+});
 
 if (rootElement) {
 	const root = ReactDOM.createRoot(rootElement);
@@ -14,10 +25,10 @@ if (rootElement) {
 		<React.StrictMode>
 			<React.Suspense fallback={<div>Loading...</div>}>
 				<QueryClientProvider client={queryClient}>
-					<App />
+					<ProApp />
 					<ProgressIndicator />
 				</QueryClientProvider>
 			</React.Suspense>
-		</React.StrictMode>,
+		</React.StrictMode>
 	);
 }
