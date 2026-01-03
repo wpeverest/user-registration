d4b5ca8667dcec811560c80b443868199fa51fb4
Prajjwal Poudel <iamprazol@gmail.com>
2026-01-03 16:21:47 +0545

    Fix - Fatal error due to undefined UR_MEMBERSHIP_VERSION

--- includes/3rd-party/DiviBuilder/BuilderAbstract.php
+++ includes/3rd-party/DiviBuilder/BuilderAbstract.php
@@ -110,12 +110,12 @@ class BuilderAbstract extends \ET_Builder_Module {
 			true
 		);
 
-		if ( defined( 'UR_MEMBERSHIP_VERSION' ) ) {
+		if ( defined( 'UR_VERSION' ) ) {
 			$suffix = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
 
 			wp_register_script( 'user-registration-membership-frontend-script', UR_MEMBERSHIP_JS_ASSETS_URL . '/frontend/user-registration-membership-frontend' . $suffix . '.js', array( 'jquery' ), '1.0.0', true );
 
-			wp_register_style( 'user-registration-membership-frontend-style', UR_MEMBERSHIP_CSS_ASSETS_URL . '/user-registration-membership-frontend.css', array(), UR_MEMBERSHIP_VERSION );
+			wp_register_style( 'user-registration-membership-frontend-style', UR_MEMBERSHIP_CSS_ASSETS_URL . '/user-registration-membership-frontend.css', array(), UR_VERSION );
 
 			wp_enqueue_script( 'user-registration-membership-frontend-script' );
 			wp_enqueue_style( 'user-registration-membership-frontend-style' );
