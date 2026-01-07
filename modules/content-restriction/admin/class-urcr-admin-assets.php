577868d2f7081df56b3604114ab4dc0558023b96
y000yal <yoyal.limbu@gmail.com>
2026-01-07 22:02:08 +0545

    Added date range support for registered date.

--- modules/content-restriction/admin/class-urcr-admin-assets.php
+++ modules/content-restriction/admin/class-urcr-admin-assets.php
@@ -51,6 +51,9 @@ class URCR_Admin_Assets {
 		if ( 'user-registration-content-restriction' === $this->current_page ) {
 			// Enqueue media scripts for media button functionality
 			wp_enqueue_media();
+			
+			wp_enqueue_script( 'flatpickr' );
+			
 			wp_enqueue_script( 'urcr-content-access-rule-creator' );
 		}
 	}
