6de7f09a15ea2ee8545fc127fbd90e3fe5d46602
Purshottam Nepal <prunepal3339@gmail.com>
2026-01-14 16:35:32 +0545

    Fix - undefined array key exists.

--- includes/admin/class-ur-admin-settings.php
+++ includes/admin/class-ur-admin-settings.php
@@ -1260,7 +1260,7 @@ class UR_Admin_Settings {
 												)
 											);
 
-											$settings .= '<input type="radio" name="' . esc_attr( $value['id'] ) . '" id="' . esc_attr( $value['id'] ) . '"	style="' . esc_attr( $value['css'] ) . '" class="' . esc_attr( $value['class'] ) . '" value="' . esc_attr( trim( $option_index ) ) . '" ' . implode( ' ', $custom_attributes ) . ' / ' . $checked . ' /> ';
+											$settings .= '<input type="radio" name="' . esc_attr( $value['id'] ?? '' ) . '" id="' . esc_attr( $value['id'] ?? '' ) . '"	style="' . esc_attr( $value['css'] ) . '" class="' . esc_attr( $value['class'] ) . '" value="' . esc_attr( trim( $option_index ) ) . '" ' . implode( ' ', $custom_attributes ) . ' / ' . $checked . ' /> ';
 											$settings .= '</label>';
 
 											$settings .= '</li>';
