18530483116f6c3b65e741144c902f09f995e0b9
DAnn2012 <dannunziata@hotmail.it>
2026-01-18 12:25:00 +0100

    Update class-ur-admin.php

--- includes/admin/class-ur-admin.php
+++ includes/admin/class-ur-admin.php
@@ -567,7 +567,7 @@ class UR_Admin {
 
 			if ( 'ur-template-refresh' === $action && ! empty( $templates ) ) {
 				if ( empty( $_GET['ur-template-nonce'] ) || ! wp_verify_nonce( sanitize_key( wp_unslash( $_GET['ur-template-nonce'] ) ), 'refresh' ) ) {
-					wp_die( esc_html_e( 'Could not verify nonce', 'user-registration' ) );
+					wp_die( esc_html__( 'Could not verify nonce', 'user-registration' ) );
 				}
 
 				foreach ( array( 'ur_pro_license_plan', 'ur_template_section_list' ) as $transient ) {
