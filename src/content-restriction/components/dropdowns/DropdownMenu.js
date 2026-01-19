173c5c71d9cd39efff5d17c897777ec4f9102f77
asheshmagar <ashesh.ido@gmail.com>
2026-01-19 21:15:53 +0545

    feat: show disabled option in pro

--- src/content-restriction/components/dropdowns/DropdownMenu.js
+++ src/content-restriction/components/dropdowns/DropdownMenu.js
@@ -47,38 +47,82 @@ const DropdownMenu = ({
 
 	const renderOptionItem = (option) => {
 		if (renderOption) {
-			return renderOption(option, handleOptionClick, handleKeyDown, selectedValue);
+			return renderOption(
+				option,
+				handleOptionClick,
+				handleKeyDown,
+				selectedValue
+			);
 		}
 
 		const isSelected = selectedValue === option.value;
-		const isDisabled = option.disabled || false;
+		const isDisabled = option.disabled || option.locked || false;
+		const isLocked = option.locked || false;
+
+		const Tag = isLocked ? "div" : "span";
 
 		return (
-			<span
+			<Tag
 				key={option.value}
 				role="button"
 				tabIndex={isDisabled ? -1 : 0}
 				className={`urcr-dropdown-option ${
 					isSelected ? "is-selected" : ""
-				} ${isDisabled ? "is-disabled" : ""}`}
-				onClick={(e) => handleOptionClick(option, e)}
-				onKeyDown={(e) => handleKeyDown(option, e)}
+				} ${isDisabled ? "is-disabled" : ""} ${isLocked ? "is-locked" : ""}`}
+				onClick={
+					isDisabled || isLocked
+						? undefined
+						: (e) => handleOptionClick(option, e)
+				}
+				onKeyDown={
+					isDisabled || isLocked
+						? undefined
+						: (e) => handleKeyDown(option, e)
+				}
 			>
 				{option.label}
-			</span>
+
+				{isLocked && (
+					<span>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+							stroke="currentColor"
+							strokeWidth="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+						>
+							<path d="M11.562 3.266a.5.5 0 0 1 .876 0L15.39 8.87a1 1 0 0 0 1.516.294L21.183 5.5a.5.5 0 0 1 .798.519l-2.834 10.246a1 1 0 0 1-.956.734H5.81a1 1 0 0 1-.957-.734L2.02 6.02a.5.5 0 0 1 .798-.519l4.276 3.664a1 1 0 0 0 1.516-.294z"></path>
+							<path d="M5 21h14"></path>
+						</svg>
+					</span>
+				)}
+			</Tag>
 		);
 	};
 
 	if (grouped) {
 		// Options are grouped: [{group: "Group Name", options: [...]}, ...]
 		return (
-			<div className={`urcr-dropdown-menu urcr-condition-field-dropdown-menu ${className}`}>
+			<div
+				className={`urcr-dropdown-menu urcr-condition-field-dropdown-menu ${className}`}
+			>
 				{options.map((group, groupIndex) => (
-					<div key={groupIndex} className="urcr-dropdown-group urcr-condition-field-dropdown-group">
+					<div
+						key={groupIndex}
+						className="urcr-dropdown-group urcr-condition-field-dropdown-group"
+					>
 						{group.group && (
-							<div className="urcr-dropdown-group-label urcr-condition-field-dropdown-group-label">{group.group}</div>
+							<div className="urcr-dropdown-group-label urcr-condition-field-dropdown-group-label">
+								{group.group}
+							</div>
 						)}
-						{group.options.map((option) => renderOptionItem(option, true))}
+						{group.options.map((option) =>
+							renderOptionItem(option, true)
+						)}
 					</div>
 				))}
 			</div>
