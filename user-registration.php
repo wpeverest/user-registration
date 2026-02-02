44da0ce0b9850d708a8d44e49b3feed83fdc0816
SidTGV <siddhi.sharma@themegrill.com>
2026-02-02 15:22:35 +0545

    Version bump to v5.1.1 and readme updates for public org release

--- user-registration.php
+++ user-registration.php
@@ -4,7 +4,7 @@
  * Plugin Name: User Registration & Membership
  * Plugin URI: https://wpuserregistration.com/
  * Description: The most flexible User Registration and Membership plugin for WordPress.
- * Version: 5.1.1
+ * Version: 5.1.1 
  * Author: WPEverest
  * Author URI: https://wpuserregistration.com
  * Text Domain: user-registration
