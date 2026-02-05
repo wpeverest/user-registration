6352c18014ec7730cbe1edab021317c50f8af371
Niraj Chaudhary <nirajchy2000@gmail.com>
2026-02-05 13:33:55 +0545

    Fix - Auto login for paypal

--- modules/membership/includes/Admin/Services/Paypal/PaypalService.php
+++ modules/membership/includes/Admin/Services/Paypal/PaypalService.php
@@ -443,15 +443,13 @@ class PaypalService {
 		}
 
 		$login_option = ur_get_user_login_option( $member_id );
-		$data = apply_filters( 'user_registration_membership_before_register_member', isset( $_POST['members_data'] ) ? (array) json_decode( wp_unslash( $_POST['members_data'] ), true ) : array() );
 
 		if ( 'auto_login' === $login_option ) {
 			$member_service = new MembersService();
-			$password        = isset( $data['password'] ) ? $data['password'] : '';
-			$member_service->login_member( $member_id, true, $password );
+			$member_service->login_member( $member_id, true );
 		}
 
-		update_user_meta( $member_id, 'urm_user_just_created', true );
+		delete_user_meta( $member_id, 'urm_user_just_created' );
 		ur_membership_redirect_to_thank_you_page( $member_id, $member_order );
 	}
 
