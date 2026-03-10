b152e6cd470026f7a7db86f5a72a3a3d33db15b3
som3669 <somshrestha3669@gmail.com>
2026-03-10 12:54:23 +0545

    fix migration

--- includes/functions-ur-core.php
+++ includes/functions-ur-core.php
@@ -3428,9 +3428,8 @@ if ( ! function_exists( 'ur_size_to_limit_length_migration_script' ) ) {
 	}
 }
 
-if ( ! ur_string_to_bool( get_option( 'urm_is_new_installation', false ) ) ) {
-	add_action( 'user_registration_init', 'ur_redirect_thank_you_page_migration_script' );
-}
+
+add_action( 'user_registration_init', 'ur_redirect_thank_you_page_migration_script' );
 
 if ( ! function_exists( 'ur_redirect_thank_you_page_migration_script' ) ) {
 
@@ -3441,9 +3440,6 @@ if ( ! function_exists( 'ur_redirect_thank_you_page_migration_script' ) ) {
 		}
 
 		$thank_you_page_id = get_option( 'user_registration_thank_you_page_id', '' );
-
-		error_log( print_r( "thank_you_page_id", true ) );
-		error_log( print_r( $thank_you_page_id, true ) );
 
 		$posts = get_posts(
 			array(
