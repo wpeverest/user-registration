90f4ec97de127d0345de1b224f45208c0f3f52f6
Milan Chaudhary <109399375+MILAN88888@users.noreply.github.com>
2026-05-12 12:41:11 +0545

    Tweak -  Updated strip sdk version to 19.4.0  (#1224)
    * tweak: strip sdk version
    
    * feat: update Stripe integration and enhance order handling logic
    
    * feat: update payment processing logic and handle trial status in order summaries
    
    * feat: update Stripe integration to use discounts array for coupons
    
    * feat: update StripeService to enhance payment method logging and subscription handling

--- modules/membership/includes/Admin/Services/OrderService.php
+++ modules/membership/includes/Admin/Services/OrderService.php
@@ -119,7 +119,7 @@ class OrderService {
 			'created_by'      => isset( $current_user ) && $current_user->ID != 0 ? $current_user->ID : absint( $member_id ),
 			'transaction_id'  => $transaction_id,
 			'payment_method'  => ( $data['membership_data']['payment_method'] ) ? sanitize_text_field( $data['membership_data']['payment_method'] ) : '',
-			'total_amount'    => ! empty( $upgrade_details ) ? $upgrade_details['chargeable_amount'] : $total,
+			'total_amount'    => ( ! empty( $upgrade_details ) && 0.0 !== (float) $total ) ? $upgrade_details['chargeable_amount'] : $total,
 			'status'          => ( 'free' === $membership_meta['type'] || $is_admin ) ? 'completed' : 'pending',
 			'order_type'      => $order_type,
 			'trial_status'    => ( ! empty( $upgrade_details ) && ( 'on' === $upgrade_details['trial_status'] ) ) ? 'on' : ( isset( $membership_meta['trial_status'] ) ? sanitize_text_field( $membership_meta['trial_status'] ) : 'off' ),
